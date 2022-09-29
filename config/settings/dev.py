ba132d35e992a0c568fe9980a4e2d57d2b1d499d
Sébastien Reuiller <sebastien@reuiller.email>
2022-09-29 17:59:05 +0200

    add django docker image

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -7,6 +7,7 @@ from .base import *  # pylint: disable=wildcard-import,unused-wildcard-import # 
 # ---------------
 SECRET_KEY = "foobar"
 
+
 DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "192.168.0.1"]
@@ -18,6 +19,16 @@ SESSION_COOKIE_SECURE = False
 CSRF_COOKIE_SECURE = False
 
 AUTH_PASSWORD_VALIDATORS = []  # Avoid password strength validation in DEV.
+
+# Django-extensions.
+# ------------------------------------------------------------------------------
+
+INSTALLED_APPS += ["django_extensions"]  # noqa F405
+
+# Django-debug-toolbar.
+# ------------------------------------------------------------------------------
+
+INSTALLED_APPS += ["debug_toolbar"]  # noqa F405
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
---
+++ docker/django/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+while ! pg_isready -h $POSTGRESQL_ADDON_HOST -p $POSTGRES_PORT; do
+    >&2 echo "Postgres is unavailable - sleeping"
+    sleep 1
+done
+
+>&2 echo "Postgres is up - continuing"
+
+django-admin migrate
+
+# --nopin disables for you the annoying PIN security prompt on the web
+# debugger. For local dev only of course!
+django-admin runserver_plus 0.0.0.0:8000 --nopin
+
+exec "$@"
