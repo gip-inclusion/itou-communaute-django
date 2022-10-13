be05c709d46927ec74180397a6b68507838c2f0e
Sébastien Reuiller <sebastien@reuiller.email>
2022-10-13 15:27:00 +0200

    add static helper to serve uploaded file in dev env

--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from machina import urls as machina_urls
@@ -14,7 +15,7 @@ urlpatterns = [
     path("members/", include(forum_member_urls)),
     # machina legacy
     path("forum/", include(machina_urls)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG and "debug_toolbar" in settings.INSTALLED_APPS:
     import debug_toolbar
