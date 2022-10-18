f7180215c0a5492d0026fa669aa9c49ab4ca4ede
vincent porte <vincent@neuralia.co>
2022-10-18 12:56:38 +0200

    feat(forum_conversation.polls): overload app and its migrations

---
+++ lacommunaute/forum_conversation/forum_polls/models.py
@@ -0,0 +1,17 @@
+from machina.apps.forum_conversation.forum_polls.abstract_models import (
+    AbstractTopicPoll,
+    AbstractTopicPollOption,
+    AbstractTopicPollVote,
+)
+
+
+class TopicPoll(AbstractTopicPoll):
+    pass
+
+
+class TopicPollOption(AbstractTopicPollOption):
+    pass
+
+
+class TopicPollVote(AbstractTopicPollVote):
+    pass
