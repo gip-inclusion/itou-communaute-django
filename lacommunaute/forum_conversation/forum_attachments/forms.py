a24836dacc05854a38127332f0fc8e3a508b79be
vincent porte <vincent@neuralia.co>
2022-10-18 12:56:07 +0200

    feat(forum_conversation.forum_attachments): overload app and its migrations

---
+++ lacommunaute/forum_conversation/forum_attachments/forms.py
@@ -0,0 +1,1 @@
+from machina.apps.forum_conversation.forum_attachments.forms import *  # noqa: F403 F401
---
+++ lacommunaute/forum_conversation/forum_attachments/models.py
@@ -0,0 +1,5 @@
+from machina.apps.forum_conversation.forum_attachments import abstract_models
+
+
+class Attachment(abstract_models.AbstractAttachment):
+    pass
