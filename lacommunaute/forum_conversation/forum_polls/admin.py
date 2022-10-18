d6c7a99a214f2b2bb7f13ed3d10687fdc033c7e9
vincent porte <vincent@neuralia.co>
2022-10-18 12:56:38 +0200

    feat(forum_conversation.polls): overload app and its migrations

---
+++ lacommunaute/forum_conversation/forum_polls/admin.py
@@ -0,0 +1,1 @@
+from machina.apps.forum_conversation.forum_polls.admin import *  # noqa: F403 F401
---
+++ lacommunaute/forum_conversation/forum_polls/forms.py
@@ -0,0 +1,1 @@
+from machina.apps.forum_conversation.forum_polls.forms import *  # noqa: F403 F401
