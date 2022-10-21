9b52c734a2dda40e0a46dc8e2769a7eddb28a76a
vincent porte <vincent@neuralia.co>
2022-10-21 17:09:15 +0200

    feat(feed): hide subject when answering post

--- lacommunaute/forum_conversation/forms.py
+++ lacommunaute/forum_conversation/forms.py
@@ -1,8 +1,18 @@
 from django.db.models import F
+from django.forms import CharField, HiddenInput
 from machina.apps.forum_conversation.forms import PostForm as AbstractPostForm
+from machina.conf import settings as machina_settings
 
 
 class PostForm(AbstractPostForm):
+    subject = CharField(widget=HiddenInput(), required=False)
+
+    def create_post(self):
+        post = super().create_post()
+        post.subject = (
+            f"{machina_settings.TOPIC_ANSWER_SUBJECT_PREFIX} {self.topic.subject}"
+        )
+        return post
 
     def update_post(self, post):
         if self.user.is_anonymous:
