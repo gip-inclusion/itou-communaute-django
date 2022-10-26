deee4598f30976ca571aa658270a0f357cf8394b
vincent porte <vincent@neuralia.co>
2022-10-26 12:31:55 +0200

    feat(logs): adding logger in customized views

--- lacommunaute/forum/views.py
+++ lacommunaute/forum/views.py
@@ -1,7 +1,11 @@
+import logging
+
 from django.conf import settings
 from machina.apps.forum.views import ForumView as BaseForumView
 from machina.core.db.models import get_model
 
+
+logger = logging.getLogger(__name__)
 
 Topic = get_model("forum_conversation", "Topic")
 
--- lacommunaute/www/forum_member/views.py
+++ lacommunaute/www/forum_member/views.py
@@ -1,5 +1,10 @@
+import logging
+
 from django.views.generic import ListView
 from machina.core.db.models import get_model
+
+
+logger = logging.getLogger(__name__)
 
 
 ForumProfile = get_model("forum_member", "ForumProfile")
