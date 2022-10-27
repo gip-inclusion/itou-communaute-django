a1f02ee1b8cb5502b6d45545fbfb092eb44ccc9c
hellodeloo <hello@hellodeloo.com>
2022-10-27 18:46:34 +0200

    feat: add error pages

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 from machina import urls as machina_urls
 
 from lacommunaute.www.forum_member import urls as forum_member_urls
@@ -26,3 +27,9 @@ if settings.DEBUG and "debug_toolbar" in settings.INSTALLED_APPS:
     import debug_toolbar
 
     urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
+
+# error pages
+handler400 = TemplateView.as_view(template_name="pages/400.html")
+handler403 = TemplateView.as_view(template_name="pages/403.html")
+handler404 = TemplateView.as_view(template_name="pages/404.html")
+handler500 = TemplateView.as_view(template_name="pages/500.html")
