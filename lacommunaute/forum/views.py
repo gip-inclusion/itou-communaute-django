7532ac8141218e24ed33f408696d1c6e42e26a5f
vincent porte <vincent@neuralia.co>
2022-10-31 12:06:08 +0100

    feat(forum_conversation): update queryset to get first Post of each Topic, plus 5 newest Post of each Topic, get one queryset with all these Post, get Topic of the forum and Prefetch only our custom queryset of Post

--- lacommunaute/forum/views.py
+++ lacommunaute/forum/views.py
@@ -1,6 +1,7 @@
 import logging
 
 from django.conf import settings
+from django.db.models import OuterRef, Prefetch, Q, Subquery
 from machina.apps.forum.views import ForumView as BaseForumView
 from machina.core.db.models import get_model
 
@@ -8,23 +9,33 @@ from machina.core.db.models import get_model
 logger = logging.getLogger(__name__)
 
 Topic = get_model("forum_conversation", "Topic")
+Post = get_model("forum_conversation", "Post")
 
 
 class ForumView(BaseForumView):
     paginate_by = settings.FORUM_TOPICS_NUMBER_PER_PAGE
 
     def get_queryset(self):
-        """Returns the list of items for this view."""
         self.forum = self.get_forum()
-        qs = (
+
+        # use slicing ([:1]) instead on .first() methode to get a queryset.
+        newest_sq = Post.objects.filter(topic=OuterRef("topic"), approved=True).values("id").order_by("created")[:1]
+        latest_sq = (
+            Post.objects.filter(topic=OuterRef("topic"), approved=True)
+            .values("id")
+            .order_by("-created")[: settings.FORUM_NUMBER_POSTS_PER_TOPIC]
+        )
+        posts = (
+            Post.objects.filter(topic__forum=self.forum)
+            .exclude(topic__type=Topic.TOPIC_ANNOUNCE)
+            .exclude(topic__approved=False)
+            .filter(Q(id__in=Subquery(newest_sq)) | Q(id__in=Subquery(latest_sq)))
+            .order_by("topic", "created")
+            .select_related("poster", "poster__forum_profile")
+            .prefetch_related("attachments")
+        )
+        return (
             self.forum.topics.exclude(type=Topic.TOPIC_ANNOUNCE)
             .exclude(approved=False)
-            .select_related("poster", "poster__forum_profile")
-            .prefetch_related(
-                "posts",
-                "posts__poster",
-                "posts__poster__forum_profile",
-                "posts__attachments",
-            )
+            .prefetch_related(Prefetch("posts", queryset=posts))
         )
-        return qs
