ed60e2f2a43223f2a4c43ad969158f1fcbb6d5fb
Sébastien Reuiller <sebastien@reuiller.email>
2022-11-02 10:08:34 +0100

    add context processor to push settings on template

--- lacommunaute/utils/urls.py
+++ lacommunaute/utils/urls.py
@@ -4,4 +4,4 @@ from django.conf import settings
 def get_absolute_url(path=""):
     if path.startswith("/"):
         path = path[1:]
-    return f"{settings.ITOU_PROTOCOL}://{settings.ITOU_FQDN}/{path}"
+    return f"{settings.COMMU_PROTOCOL}://{settings.COMMU_FQDN}/{path}"
