f83f3e549695f743b15d6d6e887f572bffabcfb5
vincent porte <vincent@neuralia.co>
2022-11-03 15:52:21 +0100

    feat(forum_member): update urls with invitation_token as kwarg

--- lacommunaute/www/forum_member/urls.py
+++ lacommunaute/www/forum_member/urls.py
@@ -7,6 +7,6 @@ app_name = "members"
 
 urlpatterns = [
     path("", ForumProfileListView.as_view(), name="profiles"),
-    path("join-forum-landing/", JoinForumLandingView.as_view(), name="join_forum_landing"),
-    path("join-forum/", JoinForumFormView.as_view(), name="join_forum_form"),
+    path("join-forum-landing/<uuid:token>/", JoinForumLandingView.as_view(), name="join_forum_landing"),
+    path("join-forum/<uuid:token>/", JoinForumFormView.as_view(), name="join_forum_form"),
 ]
