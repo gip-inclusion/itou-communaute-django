1b602ec864238895ec5971eace18ca532de0eb5d
Sébastien Reuiller <sebastien@reuiller.email>
2022-11-03 15:54:50 +0100

    better landing page with direct sign in link

--- lacommunaute/www/forum_member/views.py
+++ lacommunaute/www/forum_member/views.py
@@ -1,10 +1,12 @@
 import logging
 
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.urls import reverse_lazy
+from django.urls import reverse, reverse_lazy
+from django.utils.http import urlencode
 from django.views.generic import FormView, ListView, TemplateView
 from machina.core.db.models import get_model
 
+from lacommunaute.utils.urls import get_safe_url
 from lacommunaute.www.forum_member.forms import JoinForumForm
 
 
@@ -24,6 +26,15 @@ class ForumProfileListView(ListView):
 class JoinForumLandingView(TemplateView):
 
     template_name = "forum_member/join_forum_landing.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        params = {
+            "previous_url": reverse_lazy("members:join_forum_landing"),
+            "next_url": get_safe_url(self.request, "next"),
+        }
+        context["inclusion_connect_url"] = f"{reverse('inclusion_connect:authorize')}?{urlencode(params)}"
+        return context
 
 
 class JoinForumFormView(LoginRequiredMixin, FormView):
