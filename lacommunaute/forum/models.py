d340aa0810595adcf10279e4bf497f200bd3cd23
vincent porte <vincent@neuralia.co>
2022-11-03 10:48:58 +0100

    feat(forum): add members_group and invitation_token to prepare members management

--- lacommunaute/forum/models.py
+++ lacommunaute/forum/models.py
@@ -1,5 +1,10 @@
+import uuid
+
+from django.contrib.auth.models import Group
+from django.db import models
 from machina.apps.forum.abstract_models import AbstractForum
 
 
 class Forum(AbstractForum):
-    pass
+    members_group = models.ForeignKey(Group, null=True, on_delete=models.CASCADE, verbose_name=("Members Group"))
+    invitation_token = models.UUIDField(default=uuid.uuid4)
