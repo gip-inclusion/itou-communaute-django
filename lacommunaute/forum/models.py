69b5c4ccc1b5a239f834f182b2f304f27ce8cded
vincent porte <vincent@neuralia.co>
2022-11-07 17:55:02 +0100

    feat(forum): add is_highlighted and is_private for homepage dynamic content purposes

--- lacommunaute/forum/models.py
+++ lacommunaute/forum/models.py
@@ -8,3 +8,5 @@ from machina.apps.forum.abstract_models import AbstractForum
 class Forum(AbstractForum):
     members_group = models.ForeignKey(Group, null=True, on_delete=models.CASCADE, verbose_name=("Members Group"))
     invitation_token = models.UUIDField(default=uuid.uuid4, unique=True)
+    is_highlighted = models.BooleanField(default=False, verbose_name="affichée sur la homepage")
+    is_private = models.BooleanField(default=False, verbose_name="privée")
