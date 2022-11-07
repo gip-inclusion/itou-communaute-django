898d0c2371b766d33575e85b46d9c18bbdf9f1a9
vincent porte <vincent@neuralia.co>
2022-11-07 18:02:38 +0100

    feat(home): show highlighted forums

--- lacommunaute/www/pages/views.py
+++ lacommunaute/www/pages/views.py
@@ -1,13 +1,19 @@
 import logging
 
 from django.shortcuts import render
+from django.views.generic import ListView
+from machina.core.db.models import get_model
 
 
 logger = logging.getLogger(__name__)
 
+Forum = get_model("forum", "Forum")
 
-def home(request):
-    return render(request, "pages/home.html")
+
+class HomeListView(ListView):
+    template_name = "pages/home.html"
+    queryset = Forum.objects.filter(is_highlighted=True)
+    context_object_name = "forums"
 
 
 def contact(request):
