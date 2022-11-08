413e26b067f4fda6def186ead7ee074fbf552cf0
vincent porte <vincent@neuralia.co>
2022-11-08 16:48:25 +0100

    feat(factories): update UserFactory to solve uniqueness AND login with username in dev

--- lacommunaute/users/factories.py
+++ lacommunaute/users/factories.py
@@ -1,3 +1,5 @@
+import random
+
 import factory
 from machina.core.db.models import get_model
 
@@ -11,8 +13,10 @@ class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = User
 
-    username = factory.Sequence("username{0}".format)
+    username = factory.LazyAttribute(
+        lambda a: "{}_{}_{}".format(a.first_name, a.last_name, random.randrange(111, 999))
+    )
     first_name = factory.Faker("first_name")
     last_name = factory.Faker("last_name")
-    email = factory.Sequence("user{0}@neuralia.co".format)
+    email = factory.LazyAttribute(lambda a: "{}.{}@neuralia.co".format(a.first_name, a.last_name).lower())
     password = factory.PostGenerationMethodCall("set_password", DEFAULT_PASSWORD)
