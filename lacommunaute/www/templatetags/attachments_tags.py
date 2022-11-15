f9b690fe03a29567a75d7e7e1257fe7536359608
vincent porte <vincent@neuralia.co>
2022-11-15 16:03:17 +0100

    show attached images in post content

---
+++ lacommunaute/www/templatetags/attachments_tags.py
@@ -0,0 +1,15 @@
+from mimetypes import guess_type
+
+from django import template
+
+
+register = template.Library()
+
+
+@register.filter
+def is_image(object):
+    content_type, _ = guess_type(object.file.name)
+    if content_type in ["image/png", "image/jpeg"]:
+        return True
+    else:
+        return False
