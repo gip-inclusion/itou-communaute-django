ebcc3ec470cb9e78440bbeb38d96a0b1261cf54b
vincent porte <vincent@neuralia.co>
2022-11-16 13:52:12 +0100

    settings_context_processor: remove BASE_TEMPLATE logic

--- lacommunaute/utils/settings_context_processors.py
+++ lacommunaute/utils/settings_context_processors.py
@@ -6,10 +6,8 @@ def expose_settings(request):
     Put things into the context to make them available in templates.
     https://docs.djangoproject.com/en/4.1/ref/templates/api/#using-requestcontext
     """
-    base_template = "layouts/base_htmx.html" if request.htmx else "layouts/base.html"
 
     return {
-        "BASE_TEMPLATE": base_template,
         "ALLOWED_HOSTS": settings.ALLOWED_HOSTS,
         "COMMU_ENVIRONMENT": settings.COMMU_ENVIRONMENT,
         "COMMU_FQDN": settings.COMMU_FQDN,
