a198f15490b69f465758e3aa69249ad8b9f363f9
vincent porte <vincent@neuralia.co>
2022-11-17 15:08:40 +0100

    tests: fix and refactor utils.tests

--- lacommunaute/utils/tests.py
+++ lacommunaute/utils/tests.py
@@ -1,5 +1,7 @@
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.template import Context, Template
 from django.test import TestCase, override_settings
+from django.utils.encoding import force_bytes
 from machina.test.factories.attachments import AttachmentFactory
 from machina.test.factories.conversation import PostFactory, create_topic
 from machina.test.factories.forum import create_forum
@@ -13,10 +15,12 @@ class AttachmentsTemplateTagTests(TestCase):
         poster = UserFactory()
         cls.post = PostFactory(topic=create_topic(forum=create_forum(), poster=poster), poster=poster)
 
+    @override_settings(DEFAULT_FILE_STORAGE="django.core.files.storage.FileSystemStorage")
     def test_is_an_image(self):
         for filename in ["test.png", "test.jpg", "test.JPG", "test.jpeg", "test.JPEG"]:
             with self.subTest(filename=filename):
-                attachment = AttachmentFactory(post=self.post, file__filename=filename)
+                f = SimpleUploadedFile(filename, force_bytes("file_content"))
+                attachment = AttachmentFactory(post=self.post, file=f)
 
                 out = Template("{% load attachments_tags %}" "{{ attachment|is_image }}").render(
                     Context(
@@ -27,10 +31,12 @@ class AttachmentsTemplateTagTests(TestCase):
                 )
                 self.assertEqual(out, "True")
 
+    @override_settings(DEFAULT_FILE_STORAGE="django.core.files.storage.FileSystemStorage")
     def test_is_not_an_image(self):
         for filename in ["test.csv", "test.xlsx", "test.pdf", "test.html"]:
             with self.subTest(filename=filename):
-                attachment = AttachmentFactory(post=self.post, file__filename=filename)
+                f = SimpleUploadedFile(filename, force_bytes("file_content"))
+                attachment = AttachmentFactory(post=self.post, file=f)
 
                 out = Template("{% load attachments_tags %}" "{{ attachment|is_image }}").render(
                     Context(
@@ -45,18 +51,18 @@ class AttachmentsTemplateTagTests(TestCase):
 class SettingsContextProcessorsTest(TestCase):
     @override_settings(ALLOWED_HOSTS=["allowed.com"])
     def test_disallowed_host(self):
-        headers = {"Host": "disallowed.com"}
-        response = self.client.get("/", headers=headers)
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 400)
         self.assertFalse(hasattr(response.wsgi_request, "htmx"))
-        self.assertTemplateUsed(response, "400.html")
 
     def test_allowed_host(self):
         response = self.client.get("/")
-        self.assertTrue(hasattr(response.wsgi_request, "htmx"))
         self.assertEqual(response.status_code, 200)
+        self.assertTrue(hasattr(response.wsgi_request, "htmx"))
 
     def test_htmx_request(self):
         headers = {"HX-Request": True}
-        response = self.client.post("/", headers=headers)
+        response = self.client.get("/", headers=headers)
+        self.assertEqual(response.status_code, 200)
         self.assertTrue(hasattr(response.wsgi_request, "htmx"))
         self.assertEqual(response.status_code, 405)
