728806cccc95ada80495517fce22d303f576c318
vincent porte <vincent@neuralia.co>
2022-11-29 13:00:06 +0100

    feat(moderators pages): add forum members views, template, url and tests

--- lacommunaute/www/forum_member/views.py
+++ lacommunaute/www/forum_member/views.py
@@ -6,6 +6,7 @@ from django.urls import reverse
 from django.utils.http import urlencode
 from django.views.generic import FormView, ListView, TemplateView
 from machina.core.db.models import get_model
+from machina.core.loading import get_class
 
 from lacommunaute.utils.urls import get_safe_url
 from lacommunaute.www.forum_member.forms import JoinForumForm
@@ -16,12 +17,43 @@ logger = logging.getLogger(__name__)
 
 ForumProfile = get_model("forum_member", "ForumProfile")
 Forum = get_model("forum", "Forum")
+PermissionRequiredMixin = get_class("forum_permission.viewmixins", "PermissionRequiredMixin")
 
 
 class ForumProfileListView(ListView):
     model = ForumProfile
     template_name = "forum_member/profiles.html"
     context_object_name = "forum_profiles"
+
+
+class ModeratorProfileListView(PermissionRequiredMixin, ListView):
+    model = ForumProfile
+    template_name = "forum_member/moderator_profiles.html"
+    context_object_name = "forum_profiles"
+    paginate_by = 60
+    permission_required = ["can_approve_posts"]
+
+    def get_forum(self):
+        """Returns the forum to consider."""
+        if not hasattr(self, "forum"):
+            self.forum = get_object_or_404(Forum, pk=self.kwargs["pk"])
+        return self.forum
+
+    def get_queryset(self):
+        """Returns the list of items for this view."""
+        self.forum = self.get_forum()
+        users = self.forum.members_group.user_set.all()
+        return ForumProfile.objects.filter(user__in=users).order_by("user__first_name")
+
+    def get_controlled_object(self):
+        """Returns the controlled object."""
+        return self.get_forum()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        forum = self.get_forum()
+        context["forum"] = forum
+        return context
 
 
 class JoinForumLandingView(TemplateView):
