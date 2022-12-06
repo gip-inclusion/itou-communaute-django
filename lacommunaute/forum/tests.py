a4321cf6a08339b7b08281cddce0002c6d8f4c75
vincent porte <vincent@neuralia.co>
2022-12-06 17:48:51 +0100

    feat(forum): remove post from topic list in forum_detail, add 2 links to topic detail

--- lacommunaute/forum/tests.py
+++ lacommunaute/forum/tests.py
@@ -5,17 +5,19 @@ from django.test import RequestFactory, TestCase
 from django.urls import reverse
 from django.utils import timezone
 from django.utils.http import urlencode
+from faker import Faker
 from machina.core.db.models import get_model
 from machina.core.loading import get_class
 from machina.test.factories.conversation import create_topic
 from machina.test.factories.forum import create_forum
-from machina.test.factories.permission import UserForumPermissionFactory
 from machina.test.factories.polls import TopicPollFactory, TopicPollOptionFactory
 
 from lacommunaute.forum.views import ForumView
 from lacommunaute.forum_conversation.factories import PostFactory, TopicFactory
 from lacommunaute.users.factories import UserFactory
 
+
+faker = Faker()
 
 Topic = get_model("forum_conversation", "Topic")
 Post = get_model("forum_conversation", "Post")
@@ -60,60 +62,6 @@ class ForumViewQuerysetTest(TestCase):
 
     def test_pagination(self):
         self.assertEqual(10, self.view.paginate_by)
-
-    def test_numqueries(self):
-        poster = UserFactory()
-        topics = TopicFactory.create_batch(10, forum=self.forum, poster=poster)
-        _ = (PostFactory.create_batch(5, topic=topic, poster=poster) for topic in topics)
-
-        UserForumPermissionFactory(
-            permission=ForumPermission.objects.get(codename="can_see_forum"),
-            forum=self.forum,
-            user=self.user,
-            has_perm=True,
-        )
-        UserForumPermissionFactory(
-            permission=ForumPermission.objects.get(codename="can_read_forum"),
-            forum=self.forum,
-            user=self.user,
-            has_perm=True,
-        )
-
-        url = reverse("forum:forum", kwargs={"pk": self.forum.pk, "slug": self.forum.slug})
-
-        self.client.force_login(self.user)
-
-        # TODO fix vincentporte :
-        # view to be optimized again soon
-        with self.assertNumQueries(21):
-            response = self.client.get(url)
-
-        self.assertEqual(response.status_code, 200)
-
-    def test_get_first_and_last_n_posts(self):
-        n = 5
-
-        topic = TopicFactory(forum=self.forum, poster=self.user)
-        first_post = PostFactory(topic=topic, poster=self.user, content="first post")
-        qs = self.view.get_queryset()
-        self.assertEqual(qs.first().posts.all().count(), 1)
-        self.assertEqual(qs.first().posts.first(), first_post)
-
-        second_post = PostFactory(topic=topic, poster=self.user, content="second post")
-        qs = self.view.get_queryset()
-        self.assertEqual(qs.first().posts.all().count(), 2)
-        self.assertEqual(qs.first().posts.first(), first_post)
-        self.assertIn(second_post, qs.first().posts.all())
-
-        PostFactory.create_batch(2 * n, topic=topic, poster=self.user)
-        qs = self.view.get_queryset()
-        self.assertGreater(topic.posts.count(), qs.first().posts.all().count())
-        self.assertEqual(qs.first().posts.all().count(), n + 1)
-        self.assertEqual(qs.first().posts.first(), first_post)
-        self.assertNotIn(second_post, qs.first().posts.all())
-        self.assertEqual(
-            list(qs.first().posts.all().reverse().values("id")[:n]), list(topic.posts.all().reverse().values("id")[:n])
-        )
 
     def test_has_liked(self):
         topic = TopicFactory(forum=self.forum, poster=self.user)
@@ -199,7 +147,7 @@ class ForumViewTest(TestCase):
         self.assertContains(response, f'<a href="{topic_url}post/create/')
         self.assertNotContains(response, f'<a href="{topic_url}"')
 
-    def test_show_more_button_visibility(self):
+    def test_show_comments(self):
         topic_url = reverse(
             "forum_conversation:topic",
             kwargs={
@@ -215,11 +163,31 @@ class ForumViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertNotContains(response, f'<a href="{topic_url}"')
 
-        PostFactory.create_batch(10, topic=self.topic, poster=self.user)
+        PostFactory.create_batch(2, topic=self.topic, poster=self.user)
         response = self.client.get(self.url)
 
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, f'<a href="{topic_url}"')
+        self.assertContains(response, f'<a href="{topic_url}" class="btn btn-link btn-sm btn-ico">')
+        self.assertContains(response, "<span>Voir les 2 commentaires</span>")
+
+    def test_show_more_content(self):
+        self.post.content = faker.paragraph(nb_sentences=30)
+        self.post.save()
+        topic_url = reverse(
+            "forum_conversation:topic",
+            kwargs={
+                "forum_pk": self.forum.pk,
+                "forum_slug": self.forum.slug,
+                "pk": self.topic.pk,
+                "slug": self.topic.slug,
+            },
+        )
+        self.client.force_login(self.user)
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, f'<a href="{topic_url}" class="btn btn-link btn-sm btn-ico">')
+        self.assertContains(response, "<span>+ voir la suite</span>")
 
     def test_join_url_is_hidden(self):
         self.client.force_login(self.user)
--- lacommunaute/forum/views.py
+++ lacommunaute/forum/views.py
@@ -1,7 +1,7 @@
 import logging
 
 from django.conf import settings
-from django.db.models import Count, Exists, OuterRef, Prefetch, Q, Subquery
+from django.db.models import Count, Exists, OuterRef
 from django.urls import reverse
 from django.utils.http import urlencode
 from machina.apps.forum.views import ForumView as BaseForumView
@@ -22,30 +22,20 @@ class ForumView(BaseForumView):
     def get_queryset(self):
         self.forum = self.get_forum()
 
-        # use slicing ([:1]) instead on .first() methode to get a queryset.
-        newest_sq = Post.objects.filter(topic=OuterRef("topic"), approved=True).values("id").order_by("created")[:1]
-        latest_sq = (
-            Post.objects.filter(topic=OuterRef("topic"), approved=True)
-            .values("id")
-            .order_by("-created")[: settings.FORUM_NUMBER_POSTS_PER_TOPIC]
-        )
-        posts = (
-            Post.objects.filter(topic__forum=self.forum)
-            .exclude(topic__type=Topic.TOPIC_ANNOUNCE)
-            .exclude(topic__approved=False)
-            .filter(Q(id__in=Subquery(newest_sq)) | Q(id__in=Subquery(latest_sq)))
-            .order_by("topic", "created")
-            .select_related("poster", "poster__forum_profile")
-            .prefetch_related("attachments")
-        )
-        return (
+        qs = (
             self.forum.topics.exclude(type=Topic.TOPIC_ANNOUNCE)
             .exclude(approved=False)
             .annotate(likes=Count("likers"))
             .annotate(has_liked=Exists(User.objects.filter(topic_likes=OuterRef("id"), id=self.request.user.id)))
-            .prefetch_related(Prefetch("posts", queryset=posts), "poll")
+            .select_related(
+                "poster",
+                "poster__forum_profile",
+                "first_post",
+            )
+            .prefetch_related("poll", "poll__options", "poll__options__votes", "posts", "posts__attachments")
             .order_by("-last_post_on")
         )
+        return qs
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
