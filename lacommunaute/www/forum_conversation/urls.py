ffd2f1820999824856f977651a640c22b1037795
vincent porte <vincent@neuralia.co>
2022-12-12 18:44:06 +0100

    feat(topic): add interactive-htmx show_me_all_the_topic_content button in community feed

--- lacommunaute/www/forum_conversation/urls.py
+++ lacommunaute/www/forum_conversation/urls.py
@@ -1,12 +1,13 @@
 from django.urls import include, path
 
-from lacommunaute.www.forum_conversation.views import PostListView, TopicLikeView
+from lacommunaute.www.forum_conversation.views import PostListView, TopicContentView, TopicLikeView
 
 
 app_name = "forum_conversation_extension"
 
 conversation_urlpatterns = [
     path("topic/<str:slug>-<int:pk>/like", TopicLikeView.as_view(), name="like_topic"),
+    path("topic/<str:slug>-<int:pk>/showmore/topic", TopicContentView.as_view(), name="showmore_topic"),
     path("topic/<str:slug>-<int:pk>/showmore/posts", PostListView.as_view(), name="showmore_posts"),
 ]
 
