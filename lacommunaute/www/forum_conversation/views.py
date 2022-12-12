1338b918b5ecb03b33cfe7c3f6dc45865df030dc
vincent porte <vincent@neuralia.co>
2022-12-12 18:42:52 +0100

    feat(post): add interactive-htmx show_me_posts button in community feed

--- lacommunaute/www/forum_conversation/views.py
+++ lacommunaute/www/forum_conversation/views.py
@@ -9,7 +9,11 @@ from machina.core.loading import get_class
 logger = logging.getLogger(__name__)
 
 Topic = get_model("forum_conversation", "Topic")
+Post = get_model("forum_conversation", "Post")
 PermissionRequiredMixin = get_class("forum_permission.viewmixins", "PermissionRequiredMixin")
+TrackingHandler = get_class("forum_tracking.handler", "TrackingHandler")
+
+track_handler = TrackingHandler()
 
 
 class TopicLikeView(PermissionRequiredMixin, View):
@@ -18,7 +22,6 @@ class TopicLikeView(PermissionRequiredMixin, View):
     ]
 
     def get_topic(self):
-        """Returns the topic to consider."""
         if not hasattr(self, "topic"):
             self.topic = get_object_or_404(
                 Topic.objects.select_related("forum").all(),
@@ -42,5 +45,37 @@ class TopicLikeView(PermissionRequiredMixin, View):
         return render(request, "forum_conversation/partials/topic_likes.html", context={"topic": topic})
 
     def get_controlled_object(self):
-        """Returns the controlled object."""
+        return self.get_topic().forum
+
+
+class PostListView(PermissionRequiredMixin, View):
+    permission_required = [
+        "can_read_forum",
+    ]
+
+    def get_topic(self):
+        if not hasattr(self, "topic"):
+            self.topic = get_object_or_404(
+                Topic.objects.select_related("forum").all(),
+                pk=self.kwargs["pk"],
+            )
+        return self.topic
+
+    def get(self, request, **kwargs):
+        topic = self.get_topic()
+        posts = (
+            Post.objects.exclude(topic__type=Topic.TOPIC_ANNOUNCE)
+            .exclude(topic__approved=False)
+            .exclude(pk=topic.first_post.pk)
+            .filter(topic=topic)
+            .order_by("created")
+            .select_related("poster", "poster__forum_profile")
+            .prefetch_related("attachments")
+        )
+
+        track_handler.mark_topic_read(topic, request.user)
+
+        return render(request, "forum_conversation/partials/posts_list.html", context={"topic": topic, "posts": posts})
+
+    def get_controlled_object(self):
         return self.get_topic().forum
