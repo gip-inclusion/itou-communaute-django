f4092261e4a3f90abeed3305269a219432239927
vincent porte <vincent@neuralia.co>
2022-12-16 16:23:59 +0100

    feat(post in feed): add view, route, tests and template for PostFeedCreateView, called with htmx

--- lacommunaute/www/forum_conversation/urls.py
+++ lacommunaute/www/forum_conversation/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 
-from lacommunaute.www.forum_conversation.views import PostListView, TopicContentView, TopicLikeView
+from lacommunaute.www.forum_conversation.views import PostFeedCreateView, PostListView, TopicContentView, TopicLikeView
 
 
 app_name = "forum_conversation_extension"
@@ -9,6 +9,7 @@ conversation_urlpatterns = [
     path("topic/<str:slug>-<int:pk>/like", TopicLikeView.as_view(), name="like_topic"),
     path("topic/<str:slug>-<int:pk>/showmore/topic", TopicContentView.as_view(), name="showmore_topic"),
     path("topic/<str:slug>-<int:pk>/showmore/posts", PostListView.as_view(), name="showmore_posts"),
+    path("topic/<str:slug>-<int:pk>/comment", PostFeedCreateView.as_view(), name="comment_topic"),
 ]
 
 urlpatterns = [
