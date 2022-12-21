63fb1ada5243e09416a12d332ece98505c893192
vincent porte <vincent@neuralia.co>
2022-12-21 08:01:38 +0100

    feat(funnel): add FunnelView

--- lacommunaute/www/forum/urls.py
+++ lacommunaute/www/forum/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from lacommunaute.www.forum.views import ModeratorEngagementView
+from lacommunaute.www.forum.views import FunnelView, ModeratorEngagementView
 
 
 app_name = "forum_extension"
@@ -8,4 +8,5 @@ app_name = "forum_extension"
 
 urlpatterns = [
     path("forum/<str:slug>-<int:pk>/engagement", ModeratorEngagementView.as_view(), name="engagement"),
+    path("forum/<str:slug>-<int:pk>/funnel", FunnelView.as_view(), name="funnel"),
 ]
