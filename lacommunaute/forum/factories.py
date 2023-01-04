750df0ce0469e7a1f8593479d1822364ae6790c1
vincent porte <vincent@neuralia.co>
2023-01-04 13:02:07 +0100

    factories: a batch of new or updated factories

--- lacommunaute/forum/factories.py
+++ lacommunaute/forum/factories.py
@@ -1,7 +1,10 @@
+import factory
 from machina.test.factories.forum import ForumFactory as BaseForumFactory
 
 from lacommunaute.forum.models import Forum
+from lacommunaute.users.factories import GroupFactory
 
 
 class ForumFactory(BaseForumFactory):
     type = Forum.FORUM_POST
+    members_group = factory.SubFactory(GroupFactory, name=factory.SelfAttribute("..name"))
---
+++ lacommunaute/forum_conversation/forum_attachments/factories.py
@@ -0,0 +1,5 @@
+from machina.test.factories.attachments import AttachmentFactory as BaseAttachmentFactory
+
+
+class AttachmentFactory(BaseAttachmentFactory):
+    pass
