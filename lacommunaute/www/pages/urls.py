8ef6dabd2cd0507ba96a1fe6c0dfb6ee8c91e627
vincent porte <vincent@neuralia.co>
2023-01-10 17:35:14 +0100

    feat(stats): update statistiques page, using TemplateView

--- lacommunaute/www/pages/urls.py
+++ lacommunaute/www/pages/urls.py
@@ -3,13 +3,12 @@ from django.urls import path
 from lacommunaute.www.pages import views
 
 
-# https://docs.djangoproject.com/en/dev/topics/http/urls/#url-namespaces-and-included-urlconfs
 app_name = "pages"
 
 urlpatterns = [
     path("", views.HomeListView.as_view(), name="home"),
     path("contact/", views.contact, name="contact"),
-    path("statistiques/", views.statistiques, name="statistiques"),
+    path("statistiques/", views.StatistiquesPageView.as_view(), name="statistiques"),
     path("accessibilite/", views.accessibilite, name="accessibilite"),
     path("sentry-debug/", views.trigger_error, name="sentry_debug"),
 ]
