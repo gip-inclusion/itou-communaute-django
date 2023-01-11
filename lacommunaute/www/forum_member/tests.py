319cdd484c75365634df871c99efcdc55fcf30ea
vincent porte <vincent@neuralia.co>
2023-01-11 17:12:50 +0100

    feat(members): remove profiles listview, never used yet

--- lacommunaute/www/forum_member/tests.py
+++ lacommunaute/www/forum_member/tests.py
@@ -7,7 +7,6 @@ from machina.core.loading import get_class
 from machina.test.factories.forum import create_forum
 
 from lacommunaute.forum_member.factories import ForumProfileFactory
-from lacommunaute.forum_member.models import ForumProfile
 from lacommunaute.users.factories import DEFAULT_PASSWORD, UserFactory
 from lacommunaute.www.forum_member.views import ForumProfileUpdateView
 
@@ -23,24 +22,6 @@ class ForumProfileUpdateViewTest(TestCase):
         view.request = RequestFactory().get("/")
         view.request.user = forum_profiles.user
         self.assertEqual(view.get_success_url(), reverse("members:profile", kwargs={"pk": forum_profiles.user.pk}))
-
-
-class ForumProfileListViewTest(TestCase):
-    def test_ForumProfileListView(self):
-        forum_profiles = ForumProfileFactory.create_batch(2)
-        response = self.client.get(reverse("members:profiles"))
-
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.context["forum_profiles"]), ForumProfile.objects.count())
-
-        for forum_profile in forum_profiles:
-            with self.subTest(forum_profile=forum_profile):
-                self.assertContains(response, forum_profile.user.first_name)
-                self.assertContains(
-                    response,
-                    # legacy machina reversed url
-                    reverse("members:profile", kwargs={"pk": forum_profile.user_id}),
-                )
 
 
 class ModeratorProfileListView(TestCase):
