b3a4d0113ab0efb4638092652ff248dcc16d2ad8
vincent porte <vincent@neuralia.co>
2023-01-12 11:32:55 +0100

    feat(forum_member): replace machina mechanism to direct refs to overriden model in factory

--- lacommunaute/forum_member/factories.py
+++ lacommunaute/forum_member/factories.py
@@ -1,10 +1,7 @@
 import factory
-from machina.core.db.models import get_model
 
+from lacommunaute.forum_member.models import ForumProfile
 from lacommunaute.users.factories import UserFactory
-
-
-ForumProfile = get_model("forum_member", "ForumProfile")
 
 
 class ForumProfileFactory(factory.django.DjangoModelFactory):
