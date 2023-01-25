ab795ebd33940e19aa21414bedc80d7f2ce30678
vincent porte <vincent@neuralia.co>
2023-01-25 18:22:18 +0100

    feat(event): add calendar css & js stuffs

---
+++ lacommunaute/static/vendor/tiva-calendar/js/config.js
@@ -0,0 +1,3 @@
+// Path of events source
+var events_json = "events/events.json";
+var events_php = "events/events.php";
---
+++ lacommunaute/static/vendor/tiva-calendar/languages/en.js
@@ -0,0 +1,20 @@
+// Day, Month
+var wordMonth = new Array("Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre");
+var wordDay_sun = "Dimanche";
+var wordDay_mon = "Lundi";
+var wordDay_tue = "Mardi";
+var wordDay_wed = "Mercredi";
+var wordDay_thu = "Jeudi";
+var wordDay_fri = "Vendredi";
+var wordDay_sat = "Samedi";
+
+// View Button
+var calendar_view = "Calendrier"
+var list_view = "Liste"
+var back = "Retour"
+
+// Calendar Button
+var prev_year = "Année Précédente"
+var prev_month = "Mois Précédent"
+var next_month = "Mois Suivant"
+var next_year = "Année Suivante"
