9f889988d5fca233a60f32d130c1417c82f9d8dc
vincent porte <vincent@neuralia.co>
2023-01-31 12:12:36 +0100

    feat(forum): remove is_highlighted method

--- lacommunaute/forum/admin.py
+++ lacommunaute/forum/admin.py
@@ -6,7 +6,6 @@ class ForumAdmin(BaseForumAdmin):
     fieldsets[1][1]["fields"] += (
         "members_group",
         "invitation_token",
-        "is_highlighted",
         "is_private",
         "target_audience",
     )
