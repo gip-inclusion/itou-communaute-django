94653e969048ec2e9de0dee96650755a2aa97ac0
vincent porte <vincent@neuralia.co>
2023-01-31 11:44:18 +0100

    feat(forum): remove HomeListView

--- lacommunaute/www/pages/tests.py
+++ lacommunaute/www/pages/tests.py
@@ -15,57 +15,10 @@ assign_perm = get_class("forum_permission.shortcuts", "assign_perm")
 
 class HomepageTest(TestCase):
     def test_home_page(self):
-        url = reverse("pages:home")
+        url = reverse("forum_extension:home")
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "pages/home.html")
-
-    def test_is_highlighted(self):
-        forum = create_forum(is_highlighted=True)
-        assign_perm("can_read_forum", AnonymousUser(), forum)
-
-        response = self.client.get(reverse("pages:home"))
-        self.assertEqual(response.status_code, 200)
-
-        self.assertContains(
-            response, reverse("forum_extension:forum", kwargs={"slug": forum.slug, "pk": forum.id}), count=1
-        )
-
-    def test_is_not_highlighted(self):
-        forum = create_forum(is_highlighted=False)
-        assign_perm("can_read_forum", AnonymousUser(), forum)
-
-        response = self.client.get(reverse("pages:home"))
-        self.assertEqual(response.status_code, 200)
-
-        self.assertNotContains(response, reverse("forum_extension:forum", kwargs={"slug": forum.slug, "pk": forum.id}))
-
-    def test_is_private(self):
-        forum = create_forum(is_private=True, is_highlighted=True)
-        assign_perm("can_read_forum", AnonymousUser(), forum)
-
-        response = self.client.get(reverse("pages:home"))
-        self.assertEqual(response.status_code, 200)
-
-        self.assertContains(response, forum.name, count=1)
-        self.assertContains(response, '<p class="text-muted">Communauté privée</p>', count=1)
-
-    def test_is_not_private(self):
-        forum = create_forum(is_private=False, is_highlighted=True)
-        assign_perm("can_read_forum", AnonymousUser(), forum)
-
-        response = self.client.get(reverse("pages:home"))
-        self.assertEqual(response.status_code, 200)
-
-        self.assertContains(response, forum.name, count=1)
-        self.assertContains(response, '<p class="text-muted">Communauté publique</p>', count=1)
-
-    def test_can_join_forum_members(self):
-        forum = create_forum(is_highlighted=True)
-
-        response = self.client.get(reverse("pages:home"))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, reverse("members:join_forum_form", kwargs={"token": forum.invitation_token}))
 
 
 class StatistiquesPageTest(TestCase):
