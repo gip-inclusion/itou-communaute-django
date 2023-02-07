fe46e518dea2f3839895c9b54e9454a76ac03dd7
vincent porte <vincent@neuralia.co>
2023-02-07 17:55:51 +0100

    feat(stats): update statistiques page view

--- lacommunaute/www/pages/views.py
+++ lacommunaute/www/pages/views.py
@@ -1,14 +1,9 @@
 import logging
 
-from django.db.models.functions import TruncMonth
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
-from lacommunaute.forum_conversation.forum_polls.models import TopicPollVote
-from lacommunaute.forum_conversation.models import Post, Topic
-from lacommunaute.forum_upvote.models import UpVote
-from lacommunaute.users.models import User
-from lacommunaute.utils.stats import count_objects_per_period, format_counts_of_objects_for_timeline_chart
+from lacommunaute.utils.json import extract_values_in_list, get_json_data
 
 
 logger = logging.getLogger(__name__)
@@ -22,16 +17,9 @@ class StatistiquesPageView(TemplateView):
     template_name = "pages/statistiques.html"
 
     def get_context_data(self, **kwargs):
-        datas = (
-            count_objects_per_period(User.objects.annotate(period=TruncMonth("date_joined")), "users")
-            + count_objects_per_period(Topic.objects.annotate(period=TruncMonth("created")), "topics")
-            + count_objects_per_period(Post.objects.annotate(period=TruncMonth("created")), "posts")
-            + count_objects_per_period(UpVote.objects.annotate(period=TruncMonth("created_at")), "upvotes")
-            + count_objects_per_period(TopicPollVote.objects.annotate(period=TruncMonth("timestamp")), "pollvotes")
-        )
 
         context = super().get_context_data(**kwargs)
-        context["stats"] = format_counts_of_objects_for_timeline_chart(datas)
+        context["stats"] = extract_values_in_list(get_json_data("./exports/stats_day.json"))
         return context
 
 
