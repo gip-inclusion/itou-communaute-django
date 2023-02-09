d48f9df176d2648d320c6a8b55bcb4d89bcf27bc
vincent porte <vincent@neuralia.co>
2023-02-09 12:06:49 +0100

    feat(groups): quick users in a group management

--- lacommunaute/users/admin.py
+++ lacommunaute/users/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
 from .models import User
 
 
 admin.site.register(User, UserAdmin)
+
+
+class GroupUserInline(admin.TabularInline):
+    model = Group.user_set.through
+    list_display = "user_set__email"
+    raw_id_fields = ("user",)
+    extra = 1
+
+
+class GroupAdmin(admin.ModelAdmin):
+    inlines = [GroupUserInline]
+
+
+admin.site.unregister(Group)
+admin.site.register(Group, GroupAdmin)
