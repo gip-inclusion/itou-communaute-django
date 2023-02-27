02fc394cc3fc30cb77cc09e90a25f9287184a50d
vincent porte <vincent@neuralia.co>
2023-02-27 18:31:38 +0100

    Feat(Stat): uses datas from Stat model in statistiques page

--- lacommunaute/www/pages/tests.py
+++ lacommunaute/www/pages/tests.py
@@ -1,6 +1,10 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.utils import timezone
 from machina.core.loading import get_class
+
+from lacommunaute.forum_stats.enums import Period
+from lacommunaute.forum_stats.factories import StatFactory
 
 
 assign_perm = get_class("forum_permission.shortcuts", "assign_perm")
@@ -17,6 +21,26 @@ class HomepageTest(TestCase):
 class StatistiquesPageTest(TestCase):
     def test_context_data(self):
         url = reverse("pages:statistiques")
+        date = timezone.now()
+        names = ["nb_unique_contributors", "nb_uniq_visitors", "nb_uniq_active_visitors", "nb_engagment_events"]
+        for name in names:
+            StatFactory(name=name, date=date)
+        other_period_stat = StatFactory(
+            period=Period.WEEK, date=date - timezone.timedelta(days=7), name="nb_unique_contributors"
+        )
+
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "pages/statistiques.html")
+
+        # expected values
+        self.assertIn("stats", response.context)
+        self.assertIn("date", response.context["stats"])
+        self.assertIn("nb_unique_contributors", response.context["stats"])
+        self.assertIn("nb_uniq_visitors", response.context["stats"])
+        self.assertIn("nb_uniq_active_visitors", response.context["stats"])
+        self.assertIn("nb_engagment_events", response.context["stats"])
+        self.assertEqual(response.context["stats"]["date"][0], date.strftime("%Y-%m-%d"))
+
+        # undesired values
+        self.assertNotIn(other_period_stat.date.strftime("%Y-%m-%d"), response.context["stats"]["date"])
