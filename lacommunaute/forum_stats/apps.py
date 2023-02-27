cb6b3a5e52a33be79d9e2020267bb8ea10507f56
vincent porte <vincent@neuralia.co>
2023-02-27 18:27:34 +0100

    Feat(Stat): declare forum_stats app to store datas for statistiques page

---
+++ lacommunaute/forum_stats/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class ForumStatsAppConfig(AppConfig):
+    label = "forum_stats"
+    name = "lacommunaute.forum_stats"
+    verbose_name = "Forum Stats"
+    verbose_name_plural = "Forum Stats"
