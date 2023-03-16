deca61a15d6596f5d55a953e05236a6a227c2781
vincent porte <vincent@neuralia.co>
2023-03-16 13:51:39 +0100

    feat(forum_upvote): add HTMX CertifiedPostView, its route and tests, to certify/uncertify a Post$

--- lacommunaute/www/forum_upvote_views/urls.py
+++ lacommunaute/www/forum_upvote_views/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from lacommunaute.www.forum_upvote_views.views import PostUpvoteView
+from lacommunaute.www.forum_upvote_views.views import CertifiedPostView, PostUpvoteView
 
 
 app_name = "forum_upvote"
 
 
 urlpatterns = [
-    path("upvote", PostUpvoteView.as_view(), name="upvote"),
+    path("upvote/", PostUpvoteView.as_view(), name="upvote"),
+    path("certify/", CertifiedPostView.as_view(), name="certify"),
 ]
