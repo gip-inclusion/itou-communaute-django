7f6a395b626ced12b32ea0db98af8c4422fef6f8
vincent porte <vincent@neuralia.co>
2023-03-20 15:46:39 +0100

    changelog

--- scripts/changelog.py
+++ scripts/changelog.py
@@ -13,10 +13,11 @@ if __name__ == "__main__":
     pull_requests = repo.get_pulls(state="closed", sort="merged_at", direction="desc")
     releases = repo.get_releases()
     last_release = releases[0]
+    print(f"Last release: {last_release.title} ({last_release.published_at})")
 
     items = []
 
-    for pr in pull_requests:
+    for pr in [pr for pr in pull_requests if pr.merged_at]:
         if pr.merged_at > last_release.published_at:
             print(f"- {pr.title} (#{pr.number})")
 
