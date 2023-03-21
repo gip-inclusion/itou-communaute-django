afffb269e9306c81b84a24bc15b1962fd86738ff
vincent porte <vincent@neuralia.co>
2023-03-21 10:13:31 +0100

    feat(notification): collect new registered users from last EmailSentTrack which kind is onboarding

--- lacommunaute/notification/utils.py
+++ lacommunaute/notification/utils.py
@@ -4,6 +4,7 @@ from django.utils.timezone import now, timedelta
 from lacommunaute.forum_conversation.models import Post
 from lacommunaute.notification.enums import EmailSentTrackKind
 from lacommunaute.notification.models import EmailSentTrack
+from lacommunaute.users.models import User
 
 
 def last_notification(kind=None):
@@ -23,4 +24,11 @@ def collect_first_replies():
             approved=True,
         )
         if post.position == 2
+    ]
+
+
+def collect_new_users_for_onboarding():
+    return [
+        (user.email, user.first_name, user.last_name)
+        for user in User.objects.filter(date_joined__gte=last_notification(kind=EmailSentTrackKind.ONBOARDING))
     ]
