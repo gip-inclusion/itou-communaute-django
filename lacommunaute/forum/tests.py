3cd6ae69995ca9921c08c969151d73fca78716be
vincent porte <vincent@neuralia.co>
2023-04-03 16:48:34 +0200

    feat(forum): setup public forum manager and get_answered_topics in Forum model

--- lacommunaute/forum/tests.py
+++ lacommunaute/forum/tests.py
@@ -6,9 +6,9 @@ from machina.core.db.models import get_model
 from machina.core.loading import get_class
 
 from lacommunaute.forum.factories import ForumFactory
+from lacommunaute.forum.models import Forum
 from lacommunaute.forum_conversation.factories import PostFactory, TopicFactory
 from lacommunaute.forum_conversation.forum_polls.factories import TopicPollVoteFactory
-from lacommunaute.forum_conversation.models import Topic
 from lacommunaute.forum_upvote.factories import UpVoteFactory
 
 
@@ -19,6 +19,17 @@ UserForumPermission = get_model("forum_permission", "UserForumPermission")
 PermissionHandler = get_class("forum_permission.handler", "PermissionHandler")
 assign_perm = get_class("forum_permission.shortcuts", "assign_perm")
 remove_perm = get_class("forum_permission.shortcuts", "remove_perm")
+
+
+class ForumManagerTest(TestCase):
+    def test_private_forum(self):
+        ForumFactory(is_private=True)
+        self.assertEqual(Forum.objects.public().count(), 0)
+
+    def test_public_forum(self):
+        forum = ForumFactory(is_private=False)
+        self.assertEqual(Forum.objects.public().count(), 1)
+        self.assertIn(forum, Forum.objects.public())
 
 
 class ForumModelTest(TestCase):
@@ -66,18 +77,17 @@ class ForumModelTest(TestCase):
             },
         )
 
+    def test_get_unanswered_topics(self):
+        topic1 = TopicFactory(forum=ForumFactory(), posts_count=1)
+        topic2 = TopicFactory(forum=ForumFactory(parent=topic1.forum), posts_count=1)
+        TopicFactory(forum=ForumFactory(), posts_count=1)
+
+        unanswered_topics = topic1.forum.get_unanswered_topics()
+        self.assertEqual(unanswered_topics.count(), 2)
+        self.assertIn(topic1, unanswered_topics)
+        self.assertIn(topic2, unanswered_topics)
+
     def test_count_unanswered_topics(self):
-        forum = ForumFactory()
-
-        TopicFactory(forum=forum, posts_count=0)
-        TopicFactory(forum=forum, posts_count=1)
-        TopicFactory(forum=forum, posts_count=2)
-
-        TopicFactory(forum=forum, posts_count=1, status=Topic.TOPIC_LOCKED)
-        TopicFactory(forum=forum, posts_count=1, type=Topic.TOPIC_ANNOUNCE)
-        TopicFactory(forum=forum, posts_count=1, approved=False)
-
-        subforum = ForumFactory(parent=forum)
-        TopicFactory(forum=subforum, posts_count=1)
-
-        self.assertEqual(forum.count_unanswered_topics, 2)
+        topic = TopicFactory(forum=ForumFactory(), posts_count=1)
+        TopicFactory(forum=ForumFactory(parent=topic.forum), posts_count=1)
+        self.assertEqual(topic.forum.count_unanswered_topics, 2)
