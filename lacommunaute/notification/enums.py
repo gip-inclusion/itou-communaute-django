26e5f79c092b852aa98fc5581d467a835d31deaa
vincent porte <vincent@neuralia.co>
2023-04-04 15:56:42 +0200

    feat(notification): update recipients management in send_email, add kind of email sent

--- lacommunaute/notification/enums.py
+++ lacommunaute/notification/enums.py
@@ -4,3 +4,4 @@ from django.db import models
 class EmailSentTrackKind(models.TextChoices):
     FIRST_REPLY = "first_reply", "Première réponse à un sujet"
     ONBOARDING = "onboarding", "Onboarding d'un nouvel utilisateur"
+    PENDING_TOPIC = "pending_topic", "Question sans réponse"
