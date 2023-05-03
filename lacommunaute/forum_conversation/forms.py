1a8b4ae7084bf117485ebdb182db9ea948021356
vincent porte <vincent@neuralia.co>
2023-05-03 14:33:22 +0200

    feat(forum_conversation): setup tags in TopicForm, initialize and save them when requested

--- lacommunaute/forum_conversation/forms.py
+++ lacommunaute/forum_conversation/forms.py
@@ -1,7 +1,9 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import F
-from django.forms import CharField, HiddenInput
-from machina.apps.forum_conversation.forms import PostForm as AbstractPostForm
+from django.forms import CharField, CheckboxSelectMultiple, HiddenInput, ModelMultipleChoiceField
+from machina.apps.forum_conversation.forms import PostForm as AbstractPostForm, TopicForm as AbstractTopicForm
 from machina.conf import settings as machina_settings
+from taggit.models import Tag
 
 
 class PostForm(AbstractPostForm):
@@ -18,3 +20,23 @@ class PostForm(AbstractPostForm):
         else:
             post.updated_by = self.user
         post.updates_count = F("updates_count") + 1
+
+
+class TopicForm(AbstractTopicForm):
+    tags = ModelMultipleChoiceField(
+        label="", queryset=Tag.objects.all(), widget=CheckboxSelectMultiple, required=False
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        try:
+            if hasattr(self.instance, "topic"):
+                self.fields["tags"].initial = self.instance.topic.tags.all()
+        except ObjectDoesNotExist:
+            pass
+
+    def save(self):
+        post = super().save()
+        post.topic.tags.set(self.cleaned_data["tags"])
+        post.topic.save()
+        return post
