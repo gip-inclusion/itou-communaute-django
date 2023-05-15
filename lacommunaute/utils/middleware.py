7dfd2492e52deb4cad225b96e889dedc0e98dab0
vincent porte <vincent@neuralia.co>
2023-05-15 16:07:06 +0200

    feat(session): setup middleware to collect default visible forums

---
+++ lacommunaute/utils/middleware.py
@@ -0,0 +1,34 @@
+from django.urls import reverse
+from django.utils.deprecation import MiddlewareMixin
+from machina.core.loading import get_class
+
+from lacommunaute.forum.models import Forum
+
+
+ForumVisibilityContentTree = get_class("forum.visibility", "ForumVisibilityContentTree")
+PermissionHandler = get_class("forum_permission.handler", "PermissionHandler")
+
+# session key
+UPPER_VISIBLE_FORUMS = "upper_visible_forums"
+
+
+def store_upper_visible_forums(request):
+    forum_visibility_content_tree = ForumVisibilityContentTree.from_forums(
+        request.forum_permission_handler.forum_list_filter(Forum.objects.all(), request.user)
+    )
+    request.session[UPPER_VISIBLE_FORUMS] = [
+        {
+            "name": forum.name,
+            "level": forum.level,
+            "url": reverse("forum_extension:forum", kwargs={"slug": forum.slug, "pk": forum.id}),
+        }
+        for forum in forum_visibility_content_tree.visible_forums
+        if forum.level <= 1
+    ]
+
+
+class VisibleForumsMiddleware(MiddlewareMixin):
+    def process_request(self, request):
+        forum_visibility_content_tree = request.session.get(UPPER_VISIBLE_FORUMS, None)
+        if not forum_visibility_content_tree:
+            store_upper_visible_forums(request)
