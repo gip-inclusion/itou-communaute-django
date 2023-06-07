758a07234b69e6e184e9b3b7e0ebad7e077bcfdc
vincent porte <vincent@neuralia.co>
2023-06-07 17:28:52 +0200

    feat(forum): add CategoryForumListView

---
+++ lacommunaute/forum/tests/test_CategoryForumListView.py
@@ -0,0 +1,28 @@
+import pytest  # noqa
+from django.urls import reverse
+
+from lacommunaute.forum.factories import ForumFactory
+from lacommunaute.forum.models import Forum
+
+
+def test_context(client, db):
+    url = reverse("forum_extension:categories")
+    response = client.get(url)
+    assert response.status_code == 200
+    assert "forum/category_forum_list.html" == response.templates[0].name
+
+
+def test_queryset(client, db):
+    forum = ForumFactory(type=Forum.FORUM_CAT)
+    unvisible_forums = (
+        ForumFactory(type=Forum.FORUM_CAT, parent=forum),
+        ForumFactory(type=Forum.FORUM_CAT, is_private=True),
+        ForumFactory(),
+        ForumFactory(type=Forum.FORUM_LINK),
+    )
+    url = reverse("forum_extension:categories")
+    response = client.get(url)
+    assert response.status_code == 200
+    assert forum in response.context_data["forums"]
+    for unvisible_forum in unvisible_forums:
+        assert unvisible_forum not in response.context_data["forums"]
--- lacommunaute/forum/urls.py
+++ lacommunaute/forum/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from lacommunaute.forum.views import ForumCreateView, ForumView, ModeratorEngagementView
+from lacommunaute.forum.views import CategoryForumListView, ForumCreateView, ForumView, ModeratorEngagementView
 
 
 app_name = "forum_extension"
@@ -10,4 +10,5 @@ urlpatterns = [
     path("forum/create/", ForumCreateView.as_view(), name="create"),
     path("forum/<str:slug>-<int:pk>/", ForumView.as_view(), name="forum"),
     path("forum/<str:slug>-<int:pk>/engagement", ModeratorEngagementView.as_view(), name="engagement"),
+    path("forum/categories/", CategoryForumListView.as_view(), name="categories"),
 ]
