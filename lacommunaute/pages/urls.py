80fce628e94b8b70698f48851a88cb9e4e696984
vincent porte <vincent@neuralia.co>
2023-06-07 11:52:31 +0200

    feat(pages): adding robots.txt

--- lacommunaute/pages/urls.py
+++ lacommunaute/pages/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic.base import TemplateView
 
 from lacommunaute.pages import views
 
@@ -11,4 +12,5 @@ urlpatterns = [
     path("accessibilite/", views.accessibilite, name="accessibilite"),
     path("landing-pages/", views.LandingPagesListView.as_view(), name="landing_pages"),
     path("sentry-debug/", views.trigger_error, name="sentry_debug"),
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain"), name="robots"),
 ]
