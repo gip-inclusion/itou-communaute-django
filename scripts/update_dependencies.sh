96da5809552646c2ad9a7a7abfbc8bcca5dfae2c
vincent porte <vincent@neuralia.co>
2023-06-12 10:37:55 +0200

    update precommit

--- scripts/update_dependencies.sh
+++ scripts/update_dependencies.sh
@@ -2,9 +2,11 @@
 echo "Hi, I'm going to update the dependencies and extract last PRS for the changelog"
 python ./scripts/changelog.py
 wait
+pre-commit autoupdate
+wait
 poetry lock
 wait
 poetry run poe export && poetry run poe export_dev
 wait
-git add requirements/dev.txt && git add requirements/base.txt && git add poetry.lock
+git add requirements/dev.txt && git add requirements/base.txt && git add poetry.lock && git add .pre-commit-config.yaml
 git commit -m "update dependencies"
