39ac9b9b7c7a97c67eaadbc04b3c8c138742607d
vincent porte <vincent@neuralia.co>
2023-06-19 17:23:25 +0200

    fix(forum_search): resilient test_update_index_daily

--- lacommunaute/forum_search/tests/test_update_index_daily.py
+++ lacommunaute/forum_search/tests/test_update_index_daily.py
@@ -1,3 +1,5 @@
+from io import StringIO
+
 import pytest
 from django.core.management import call_command
 from django.urls import reverse
@@ -11,30 +13,11 @@ def search_url():
     return reverse("forum_search_extension:search")
 
 
-@pytest.fixture
-def forum():
-    return ForumFactory(with_public_perms=True)
+def test_recent_content_is_added_to_index(client, db, search_url):
+    forum = ForumFactory(with_public_perms=True)
+    TopicFactory(forum=forum, with_post=True)
 
+    out = StringIO()
+    call_command("update_index", age=1, stdout=out, stderr=StringIO())
 
-def test_recent_updated_forum_is_added_to_index(client, db, search_url, forum):
-    call_command("update_index", age=1)
-
-    query = forum.description.raw.split()[:4]
-    response = client.get(search_url, {"q": " ".join(query)})
-
-    assert response.status_code == 200
-    for word in query:
-        assert f'<span class="highlighted">{word}</span>' in response.content.decode("utf-8")
-
-
-def test_recent_updated_post_is_added_to_index(client, db, search_url, forum):
-    topic = TopicFactory(forum=forum, with_post=True)
-
-    call_command("update_index", age=1)
-
-    query = topic.first_post.content.raw.split()[:4]
-    response = client.get(search_url, {"q": " ".join(query)})
-
-    assert response.status_code == 200
-    for word in query:
-        assert f'<span class="highlighted">{word}</span>' in response.content.decode("utf-8")
+    assert out.getvalue() == "Indexing 1 Thématiques\nIndexing 1 Messages\n"
