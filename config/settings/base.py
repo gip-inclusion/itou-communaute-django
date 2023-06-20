93825e88db935679cb6a6d706312eb64139fcc24
vincent porte <vincent@neuralia.co>
2023-06-20 16:00:41 +0200

    fix(forum_search): s3 path

--- config/settings/base.py
+++ config/settings/base.py
@@ -260,10 +260,17 @@ DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 HAYSTACK_CONNECTIONS = {
     "default": {
         "ENGINE": "haystack.backends.whoosh_backend.WhooshEngine",
-        "PATH": f"{AWS_S3_ENDPOINT_URL}/whoosh_index",
+        "PATH": f"s3://{AWS_STORAGE_BUCKET_NAME}/whoosh_index",
+        "AWS_ACCESS_KEY_ID": AWS_S3_ACCESS_KEY_ID,
+        "AWS_SECRET_ACCESS_KEY": AWS_S3_SECRET_ACCESS_KEY,
+        "AWS_STORAGE_BUCKET_NAME": AWS_STORAGE_BUCKET_NAME,
+        "AWS_S3_ENDPOINT_URL": AWS_S3_ENDPOINT_URL,
+        "AWS_S3_REGION_NAME": AWS_S3_STORAGE_BUCKET_REGION,
+        "AWS_S3_USE_SSL": True,  # Change to True if your endpoint supports SSL
+        "AWS_AUTO_CREATE_BUCKET": False,  # Set to True if you want Haystack to create the bucket
+        "AWS_AUTO_CREATE_INDEX": True,  # Set to False if you manually create the index
     },
 }
-
 # Forum - Machina settings
 # ------------------------------------------------------------------------------
 MACHINA_FORUM_NAME = "La Communauté"
