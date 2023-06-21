a7f35b6b158312d98214ab768e010ae22fb70e44
vincent porte <vincent@neuralia.co>
2023-06-21 17:55:47 +0200

    feat(notification): add FOLLOWING_REPLIES in EmailSentTrackKind enum

--- lacommunaute/notification/enums.py
+++ lacommunaute/notification/enums.py
@@ -3,5 +3,6 @@ from django.db import models
 
 class EmailSentTrackKind(models.TextChoices):
     FIRST_REPLY = "first_reply", "Première réponse à un sujet"
+    FOLLOWING_REPLIES = "following_replies", "Réponses suivantes"
     ONBOARDING = "onboarding", "Onboarding d'un nouvel utilisateur"
     PENDING_TOPIC = "pending_topic", "Question sans réponse"
