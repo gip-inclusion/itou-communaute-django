4a34fe02bcd15f7fd91951fed6d62bb1f42f2669
vincent porte <vincent@neuralia.co>
2023-06-21 18:39:49 +0200

    feat(notification): collect_following_replies

--- lacommunaute/notification/utils.py
+++ lacommunaute/notification/utils.py
@@ -1,7 +1,8 @@
 from django.conf import settings
+from django.db.models import Count, F, Q
 from django.utils.timezone import now, timedelta
 
-from lacommunaute.forum_conversation.models import Post
+from lacommunaute.forum_conversation.models import Post, Topic
 from lacommunaute.notification.enums import EmailSentTrackKind
 from lacommunaute.notification.models import BouncedEmail, EmailSentTrack
 from lacommunaute.users.models import User
@@ -27,12 +28,34 @@ def collect_first_replies():
     ]
 
 
+def collect_following_replies():
+    # vincentporte : assumed disapproved post are counted
+    return [
+        (
+            f"{settings.COMMU_PROTOCOL}://{settings.COMMU_FQDN}{topic.get_absolute_url()}",
+            topic.subject,
+            topic.poster_email,
+            f"{topic.new_replies} nouvelle réponse"
+            if topic.new_replies == 1
+            else f"{topic.new_replies} nouvelles réponses",
+        )
+        for topic in Topic.objects.filter(
+            updated__gte=last_notification(kind=EmailSentTrackKind.FOLLOWING_REPLIES), posts_count__gte=3
+        ).annotate(
+            new_replies=Count(
+                "posts",
+                filter=Q(posts__created__gte=last_notification(kind=EmailSentTrackKind.FOLLOWING_REPLIES))
+                & ~Q(posts__id=F("first_post_id")),
+            )
+        )
+    ]
+
+
 def collect_new_users_for_onboarding():
     return User.objects.filter(date_joined__gte=last_notification(kind=EmailSentTrackKind.ONBOARDING))
 
 
 def should_not_be_approved(username: str) -> bool:
-
     """
     User model stores Inclusion Connect uuid as username, email as email
     Post model stores anonymous user email as username
