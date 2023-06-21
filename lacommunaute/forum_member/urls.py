d2aaf70058c3bd6694f68fab1a5d8d70402b2389
vincent porte <vincent@neuralia.co>
2023-06-21 16:29:50 +0200

    feat(forum_member): replace pk by slug to access detail view

--- lacommunaute/forum_member/urls.py
+++ lacommunaute/forum_member/urls.py
@@ -13,7 +13,7 @@ app_name = "members"
 
 urlpatterns = [
     path("profile/edit/", ForumProfileUpdateView.as_view(), name="profile_update"),
-    path("profile/<str:pk>/", ForumProfileDetailView.as_view(), name="profile"),
+    path("profile/<str:username>/", ForumProfileDetailView.as_view(), name="profile"),
     path("forum/<str:slug>-<int:pk>/", ModeratorProfileListView.as_view(), name="forum_profiles"),
     path("join-forum-landing/<uuid:token>/", JoinForumLandingView.as_view(), name="join_forum_landing"),
     path("join-forum/<uuid:token>/", JoinForumFormView.as_view(), name="join_forum_form"),
