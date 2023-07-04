8f89b650fb6d36b5140201bd2a818cd17a72619c
vincent porte <vincent@neuralia.co>
2023-07-04 12:06:09 +0200

    feat(forum): add access to forums list to authentited users

--- lacommunaute/forum/urls.py
+++ lacommunaute/forum/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from machina.apps.forum.views import IndexView
 
 from lacommunaute.forum.views import CategoryForumListView, ForumView
 
@@ -9,4 +10,5 @@ app_name = "forum_extension"
 urlpatterns = [
     path("forum/<str:slug>-<int:pk>/", ForumView.as_view(), name="forum"),
     path("forum/documentation/", CategoryForumListView.as_view(), name="documentation"),
+    path("forum/", IndexView.as_view(), name="index"),
 ]
