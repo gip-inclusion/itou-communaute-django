7cf78bb8030837679b48df88458b7cc4a392b384
vincent porte <vincent@neuralia.co>
2023-07-18 11:13:28 +0200

    feat(home): sort last topics on creation date

--- lacommunaute/pages/tests/test_homepage.py
+++ lacommunaute/pages/tests/test_homepage.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 
 from lacommunaute.forum.enums import Kind as ForumKind
 from lacommunaute.forum.factories import ForumFactory
-from lacommunaute.forum_conversation.factories import TopicFactory
+from lacommunaute.forum_conversation.factories import PostFactory, TopicFactory
 
 
 def test_context_data(client, db):
@@ -24,3 +24,19 @@ def test_context_data(client, db):
     assert response.context_data["topics_public"].get() == topic
     assert response.context_data["topics_newsfeed"].get() == news
     assert response.context_data["forums_category"].get() == article
+
+
+def test_new_topics_order(client, db):
+    topic1 = TopicFactory(with_post=True, forum=ForumFactory())
+    topic2 = TopicFactory(with_post=True, forum=ForumFactory())
+    url = reverse("pages:home")
+
+    response = client.get(url)
+    assert response.status_code == 200
+    assert list(response.context_data["topics_public"]) == [topic2, topic1]
+
+    PostFactory(topic=topic1)
+
+    response = client.get(url)
+    assert response.status_code == 200
+    assert list(response.context_data["topics_public"]) == [topic2, topic1]
