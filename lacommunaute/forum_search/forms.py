b477b34dbdd0943d7932574f5d196515373d3e41
vincent porte <vincent@neuralia.co>
2023-07-26 11:14:40 +0200

    feat(forum_search): search by model

--- lacommunaute/forum_search/forms.py
+++ lacommunaute/forum_search/forms.py
@@ -1,14 +1,28 @@
+from django.forms import ChoiceField, RadioSelect
 from django.utils.translation import gettext_lazy as _
 from haystack.forms import FacetedSearchForm
 from machina.core.loading import get_class
+
+from lacommunaute.forum.models import Forum
+from lacommunaute.forum_conversation.models import Post
 
 
 PermissionHandler = get_class("forum_permission.handler", "PermissionHandler")
 
 
 class SearchForm(FacetedSearchForm):
+    MODEL_CHOICES = (
+        ("all", "tout le site"),
+        ("post", "les échanges"),
+        ("forum", "la documentation"),
+    )
+    m = ChoiceField(choices=MODEL_CHOICES, required=False, widget=RadioSelect)
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        self.fields["m"].label = _("Search in")
+        self.fields["m"].initial = kwargs.pop("initial", {}).get("m", "all")
 
         self.fields["q"].label = _("Search for keywords")
         self.fields["q"].widget.attrs["placeholder"] = _("Keywords or phrase")
@@ -19,5 +33,10 @@ class SearchForm(FacetedSearchForm):
 
         if not self.is_valid():
             return self.no_query_found()
+
+        if self.cleaned_data.get("m") == "post":
+            sqs = sqs.models(Post)
+        elif self.cleaned_data.get("m") == "forum":
+            sqs = sqs.models(Forum)
 
         return sqs
