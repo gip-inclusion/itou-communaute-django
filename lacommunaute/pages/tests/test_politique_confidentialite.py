8ec2077e71163eaf0c98a904165266d908ba79e3
vincent porte <vincent@neuralia.co>
2023-09-06 13:12:15 +0200

    feat(pages): adding politique_de_confidentialite

---
+++ lacommunaute/pages/tests/test_politique_confidentialite.py
@@ -0,0 +1,8 @@
+import pytest  # noqa
+from django.urls import reverse
+from pytest_django.asserts import assertTemplateUsed
+
+
+def test_template(client, db):
+    response = client.get(reverse("pages:politique_de_confidentialite"))
+    assertTemplateUsed(response, "pages/politique_de_confidentialite.html")
