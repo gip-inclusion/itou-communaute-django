a5b2f5503b21b544e13db0e8c003a0cd26bbc866
vincent porte <vincent@neuralia.co>
2023-09-14 18:36:37 +0200

    feat(surveys): init app

---
+++ lacommunaute/surveys/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class ForumStatsAppConfig(AppConfig):
+    label = "surveys"
+    name = "lacommunaute.surveys"
+    verbose_name = "Survey"
+    verbose_name_plural = "Surveys"
