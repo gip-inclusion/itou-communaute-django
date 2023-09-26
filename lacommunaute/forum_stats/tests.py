2bbfa18c21c175b10ab7d7b3c1f726c14ba66cde
vincent porte <vincent@neuralia.co>
2023-09-26 17:17:05 +0200

    feat(stats): add queryset to Stat model

--- lacommunaute/forum_stats/tests.py
+++ lacommunaute/forum_stats/tests.py
@@ -1,9 +1,12 @@
+from dateutil.relativedelta import relativedelta
 from django.db import IntegrityError
 from django.test import TestCase
 from django.utils import timezone
+from django.utils.timezone import localdate
 
 from lacommunaute.forum_stats.enums import Period
 from lacommunaute.forum_stats.factories import StatFactory
+from lacommunaute.forum_stats.models import Stat
 
 
 class StatModelTest(TestCase):
@@ -14,3 +17,17 @@ class StatModelTest(TestCase):
         StatFactory(name=name, date=date, period=period)
         with self.assertRaises(IntegrityError):
             StatFactory(name=name, date=date, period=period)
+
+
+class StatQuerySetTest(TestCase):
+    def test_ordering(self):
+        today = localdate()
+        stat = StatFactory(period=Period.MONTH, date=today)
+        StatFactory(period=Period.DAY, date=today)
+        StatFactory(period=Period.MONTH, date=today - relativedelta(months=1))
+
+        qs = Stat.objects.current_month_datas()
+        self.assertEqual(list(qs), [{"name": stat.name, "value": stat.value, "date": today}])
+
+    def test_empty_dataset(self):
+        self.assertEqual(Stat.objects.current_month_datas().count(), 0)
