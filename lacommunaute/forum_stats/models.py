c264cb1433cbd6276a621b2abc731e4ab986cb2c
vincent porte <vincent@neuralia.co>
2023-09-26 17:17:05 +0200

    feat(stats): add queryset to Stat model

--- lacommunaute/forum_stats/models.py
+++ lacommunaute/forum_stats/models.py
@@ -3,6 +3,16 @@ from django.db import models
 from lacommunaute.forum_stats.enums import Period
 
 
+class StatQuerySet(models.QuerySet):
+    def current_month_datas(self):
+        qs = Stat.objects.filter(period="month").order_by("-date")
+
+        if qs.exists():
+            return Stat.objects.filter(date=qs.first().date, period=Period.MONTH).values("name", "value", "date")
+
+        return Stat.objects.none()
+
+
 class Stat(models.Model):
     name = models.CharField(max_length=30, verbose_name="Nom")
     date = models.DateField(verbose_name="Date")
@@ -19,3 +29,5 @@ class Stat(models.Model):
 
     def __str__(self):
         return f"{self.name} - {self.date} - {self.period}"
+
+    objects = StatQuerySet().as_manager()
