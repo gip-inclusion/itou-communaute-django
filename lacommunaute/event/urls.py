87e9104334e1eda480ad3af0ad159dba2c320c53
vincent porte <vincent@neuralia.co>
2023-10-02 11:45:40 +0200

    [EVENT] del tiva-calendar stuffs

--- lacommunaute/event/urls.py
+++ lacommunaute/event/urls.py
@@ -7,8 +7,6 @@ from lacommunaute.event.views import (
     EventListView,
     EventMonthArchiveView,
     EventUpdateView,
-    calendar,
-    calendar_data,
 )
 
 
@@ -16,13 +14,11 @@ app_name = "event"
 
 
 urlpatterns = [
-    path("", calendar, name="calendar"),
     path("create/", EventCreateView.as_view(), name="create"),
     path("<int:pk>/", EventDetailView.as_view(), name="detail"),
     path("<int:pk>/update/", EventUpdateView.as_view(), name="update"),
     path("<int:pk>/delete/", EventDeleteView.as_view(), name="delete"),
     path("myevents/", EventListView.as_view(), name="myevents"),
-    path("events/events.json", calendar_data, name="data_source"),
     path("events/<int:year>/<int:month>/", EventMonthArchiveView.as_view(), name="month"),
     path("events/", EventMonthArchiveView.as_view(), name="current"),
 ]
