5e462923f973d79c05f5d45416418d16d021b2e9
vincent porte <vincent@neuralia.co>
2023-11-14 11:00:21 +0100

    remove poethepoet from update_dependencies script

--- scripts/update_dependencies.sh
+++ scripts/update_dependencies.sh
@@ -6,7 +6,9 @@ pre-commit autoupdate
 wait
 poetry lock
 wait
-poetry run poe export && poetry run poe export_dev
+poetry export -f requirements.txt --output ./requirements/base.txt
+wait
+poetry export -f requirements.txt --output ./requirements/dev.txt --with dev
 wait
 git add requirements/dev.txt && git add requirements/base.txt && git add poetry.lock && git add .pre-commit-config.yaml
 git commit -m "update dependencies"
