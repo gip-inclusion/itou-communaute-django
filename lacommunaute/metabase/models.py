64541166565209a02ef0a5823297f9e356c440ef
vincent porte <vincent@neuralia.co>
2023-12-05 17:33:18 +0100

    (metabase) add PostTable model, admin, migration, factory and tests

--- lacommunaute/metabase/models.py
+++ lacommunaute/metabase/models.py
@@ -27,3 +27,32 @@ class ForumTable(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class PostTable(models.Model):
+    subject = models.CharField(max_length=255, verbose_name="Sujet")
+    subject_likes_count = models.PositiveIntegerField(default=0, verbose_name="Nombre de likes du sujet")
+    forum_name = models.CharField(max_length=100, verbose_name="Nom du forum")
+    poster = models.CharField(max_length=64, verbose_name="Identifiant de l'auteur")
+    is_anonymous_post = models.BooleanField(default=False, verbose_name="Anonyme")
+    certifier = models.CharField(max_length=64, null=True, verbose_name="Identifiant du certifieur")
+    post_upvotes_count = models.PositiveIntegerField(default=0, verbose_name="Nombre d'upvotes")
+    attachments_count = models.PositiveIntegerField(default=0, verbose_name="Nombre de pièces jointes")
+    tags_list = models.CharField(null=True, max_length=200, verbose_name="Liste des tags")
+    approved_boolean = models.BooleanField(default=False, verbose_name="Approuvé")
+    topic_created_at = models.DateTimeField(null=True, verbose_name="Date de création du sujet")
+    post_created_at = models.DateTimeField(null=True, verbose_name="Date de création du message")
+    post_position_in_topic = models.PositiveIntegerField(default=0, verbose_name="Position du message dans le sujet")
+    updates_count = models.PositiveIntegerField(default=0, verbose_name="Nombre de mises à jour")
+    post_last_updated_at = models.DateTimeField(null=True, verbose_name="Date de dernière mise à jour")
+    extracted_at = models.DateTimeField(auto_now_add=True, verbose_name="Date d'extraction des données")
+
+    objects = models.Manager()
+
+    class Meta:
+        verbose_name = "Post Table"
+        verbose_name_plural = "Posts Table"
+        ordering = ["subject", "post_position_in_topic"]
+
+    def __str__(self):
+        return f"{self.subject} - {self.post_position_in_topic}"
