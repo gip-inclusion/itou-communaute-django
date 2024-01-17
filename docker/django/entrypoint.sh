9f6b12f679367361aa86df7b825325667f12c70e
François Freitag <mail@franek.fr>
2024-01-17 16:24:32 +0100

    Upgrade recipe to build Django container
    Don’t install Python requirements as root.
    Get closer to the recipe used on les-emplois, by specifying build args,
    using a nicer installation method for PostgreSQL.
    
    Drop unused ENV DOCKER_DEFAULT_PLATFORM=linux/amd64.

--- docker/django/entrypoint.sh
+++ docker/django/entrypoint.sh
@@ -8,7 +8,10 @@ done
 
 >&2 echo "Postgres is up - continuing"
 
-django-admin migrate
+python3 -m venv ~/venv
+. ~/venv/bin/activate
+pip install -r /app/requirements/dev.txt
+python manage.py migrate
 
 # --nopin disables for you the annoying PIN security prompt on the web
 # debugger. For local dev only of course!
