af9dbf5cbf8fb15d1df900dddfe30d90d1b92c7f
vincent porte <vincent@neuralia.co>
2024-01-18 15:50:57 +0100

    (search) add ask_a_question button on search page

--- lacommunaute/forum_search/views.py
+++ lacommunaute/forum_search/views.py
@@ -1,8 +1,15 @@
 from haystack import views
 
+from lacommunaute.forum.enums import Kind as ForumKind
+from lacommunaute.forum.models import Forum
 from lacommunaute.forum_search.forms import SearchForm
 
 
 class FacetedSearchView(views.FacetedSearchView):
     form_class = SearchForm
     template = "forum_search/search.html"
+
+    def extra_context(self):
+        extra = super().extra_context()
+        extra["forum"] = Forum.objects.filter(kind=ForumKind.PUBLIC_FORUM, lft=1, level=0).first()
+        return extra
