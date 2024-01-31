c1c185563dddd9aa3a141c380992fb322f4fa12d
François Freitag <mail@franek.fr>
2024-01-31 17:01:19 +0100

    Monitor JavaScript errors with Sentry
    Allows reporting of unhandled errors and promise rejections to Sentry.

--- config/settings/base.py
+++ config/settings/base.py
@@ -353,6 +353,9 @@ CSP_SCRIPT_SRC = (
     "https://cdn.jsdelivr.net/npm/jquery@3.6.1/dist/jquery.min.js",
     "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js",
     "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js",
+    # https://docs.sentry.io/platforms/javascript/install/loader/#content-security-policy
+    "https://browser.sentry-cdn.com",
+    "https://js.sentry-cdn.com",
     "https://tally.so",
 )
 CSP_FRAME_SRC = ("'self'", "https://tally.so")
