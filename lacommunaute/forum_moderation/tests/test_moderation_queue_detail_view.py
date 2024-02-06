70f013fb74a1218adb745266b60a44fa1620bf05
vincent porte <vincent@neuralia.co>
2024-02-06 11:21:10 +0100

    (moderation) fix moderation queued post template

---
+++ lacommunaute/forum_moderation/tests/test_moderation_queue_detail_view.py
@@ -0,0 +1,23 @@
+import pytest  # noqa
+
+from lacommunaute.forum_conversation.factories import TopicFactory, AnonymousPostFactory
+from lacommunaute.users.factories import UserFactory
+
+from django.urls import reverse
+from pytest_django.asserts import assertContains
+
+
+def test_detail_view_for_anonymous_post(client, db):
+    post = AnonymousPostFactory(topic=TopicFactory(approved=False), approved=False)
+    client.force_login(UserFactory(is_superuser=True))
+    response = client.get(reverse("forum_moderation:queued_post", kwargs={"pk": post.pk}))
+    assertContains(response, post.poster_display_name)
+    assertContains(response, post.topic.subject)
+
+
+def test_detail_view_with_authenticated_post(client, db):
+    topic = TopicFactory(approved=False, with_post=True)
+    client.force_login(UserFactory(is_superuser=True))
+    response = client.get(reverse("forum_moderation:queued_post", kwargs={"pk": topic.first_post.pk}))
+    assertContains(response, topic.subject)
+    assertContains(response, topic.first_post.poster_display_name)
