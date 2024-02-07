gip-inclusion/itou-communaute-django

commit f81390f4bef63f4c2896180fcf25fabbb2302733

Fix django-compressor errors on deploy
Django-compressor follows templates (through extends), but outside of a
request/response cycle, it lacks context coming from context processors,
in particular `BASE_TEMPLATE`.
Define it through its setting `COMPRESS_OFFLINE_CONTEXT`.

Django-machina has a swappable base template, `_base`.html, configured
through the `MACHINA_BASE_TEMPLATE_NAME` setting. Include that value in
`COMPRESS_OFFLINE_CONTEXT`.

Also, django-machina actually has a hard dependency on haystack, as it
ships the forum_search app, which templates rely on haystack highlight
template tag. When haystack is not in `INSTALLED_APPS`, django-compress
complains about an unknown highlight template tag.
Restore a dummy configuration for haystack.

Finally, the app was setup for `COMPRESS_OFFLINE=True`, the compression
happening on deployment, but that was not communicated to
django-compressor (the setting value was False). That will prevent
compressing the static assets on the request/response cycle and should
increase performance.

config/settings/base.py
@@ -2,6 +2,7 @@ import os
 
 from dotenv import load_dotenv
 from machina import MACHINA_MAIN_STATIC_DIR, MACHINA_MAIN_TEMPLATE_DIR
+from machina.conf.settings import BASE_TEMPLATE_NAME
 
 
 load_dotenv()
@@ -39,6 +40,7 @@ THIRD_PARTIES_APPS = [
     # Django Storage for S3
     "storages",
     # Machina dependencies:
+    "haystack",  # Provides search, but is unused.
     "mptt",  # to handle the tree of forum instances
     "widget_tweaks",
     # Machina apps:
@@ -226,6 +228,11 @@ STATICFILES_FINDERS = [
 ]
 
 COMPRESS_PRECOMPILERS = (("text/x-scss", "django_libsass.SassCompiler"),)
+COMPRESS_OFFLINE = True
+COMPRESS_OFFLINE_CONTEXT = {
+    "BASE_TEMPLATE": "layouts/base.html",
+    "MACHINA_BASE_TEMPLATE_NAME": BASE_TEMPLATE_NAME,
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -276,6 +283,13 @@ FORUM_TOPICS_NUMBER_PER_PAGE = 10
 FORUM_NUMBER_POSTS_PER_TOPIC = 5
 MACHINA_FORUM_IMAGE_WIDTH = 300
 MACHINA_FORUM_IMAGE_HEIGHT = 300
+
+# Actually unused, but required by Machina.
+HAYSTACK_CONNECTIONS = {
+    "default": {
+        "ENGINE": "haystack.backends.simple_backend.SimpleEngine",
+    },
+}
 
 # Inclusion Connect
 # ------------------------------------------------------------------------------

.github/workflows/ci.yml
@@ -52,7 +52,9 @@ jobs:
       - name: ✨ Black, ruff & djlint
         run: make quality
       - name: 🚧 collect static for testing purposes
-        run: python manage.py collectstatic
+        run: |
+          python manage.py collectstatic
+          python manage.py compress
       - name: 🚧 Check pending migrations
         run: python manage.py makemigrations --check --dry-run --noinput
       - name: 🤹‍ Django tests

commit f43f0698f6e289018f259fcbeecab51f48a487f8

Fix <title> on the site
Regression introduced in f81390f4bef63f4c2896180fcf25fabbb2302733.

config/settings/base.py
@@ -2,7 +2,6 @@ import os
 
 from dotenv import load_dotenv
 from machina import MACHINA_MAIN_STATIC_DIR, MACHINA_MAIN_TEMPLATE_DIR
-from machina.conf.settings import BASE_TEMPLATE_NAME
 
 
 load_dotenv()
@@ -231,7 +230,10 @@ COMPRESS_PRECOMPILERS = (("text/x-scss", "django_libsass.SassCompiler"),)
 COMPRESS_OFFLINE = True
 COMPRESS_OFFLINE_CONTEXT = {
     "BASE_TEMPLATE": "layouts/base.html",
-    "MACHINA_BASE_TEMPLATE_NAME": BASE_TEMPLATE_NAME,
+    # Do not import machina settings for that value. It forces evaluation
+    # of machina’s getattr(settings, "XXX", MACHINA_DEFAULT) before our
+    # settings have be defined, thus preventing our overrides.
+    "MACHINA_BASE_TEMPLATE_NAME": "_base.html",
 }
 
 # Default primary key field type

lacommunaute/pages/tests/test_homepage.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from lacommunaute.forum.enums import Kind as ForumKind
 from lacommunaute.forum.factories import ForumFactory
 from lacommunaute.forum_conversation.factories import PostFactory, TopicFactory
+from pytest_django.asserts import assertContains
 
 
 def test_context_data(client, db):
@@ -40,3 +41,8 @@ def test_new_topics_order(client, db):
     response = client.get(url)
     assert response.status_code == 200
     assert list(response.context_data["topics_public"]) == [topic2, topic1]
+
+
+def test_page_title(db, client):
+    response = client.get(reverse("pages:home"))
+    assertContains(response, "<title>Accueil- La communauté de l'inclusion</title>", html=True, count=1)