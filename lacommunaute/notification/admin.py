3d943cd92be913e9fc595c0e54b15704d9a42077
vincent porte <vincent@neuralia.co>
2024-02-07 17:23:43 +0100

    (notification) rearrange notification admin registration

--- lacommunaute/notification/admin.py
+++ lacommunaute/notification/admin.py
@@ -3,15 +3,13 @@ from django.contrib import admin
 from lacommunaute.notification.models import BouncedEmail, EmailSentTrack
 
 
+@admin.register(EmailSentTrack)
 class EmailSentTrackAdmin(admin.ModelAdmin):
     list_display = ("kind", "created", "status_code")
     list_filter = ("kind",)
 
 
+@admin.register(BouncedEmail)
 class BouncedEmailAdmin(admin.ModelAdmin):
     list_display = ("email", "created", "reason")
     list_filter = ("reason",)
-
-
-admin.site.register(EmailSentTrack, EmailSentTrackAdmin)
-admin.site.register(BouncedEmail, BouncedEmailAdmin)
