e9f46b1dd45110b7139c140e97676a40eddc0eb5
vincent porte <vincent@neuralia.co>
2024-02-12 10:25:12 +0100

    (notification) lowerize verbose names, reword BouncedEmail verbose name for more consistency

--- lacommunaute/notification/models.py
+++ lacommunaute/notification/models.py
@@ -5,28 +5,28 @@ from lacommunaute.notification.enums import EmailSentTrackKind
 
 
 class EmailSentTrack(DatedModel):
-    status_code = models.IntegerField(verbose_name="Code de retour de l'API")
-    response = models.TextField(verbose_name="Réponse de l'API")
-    datas = models.JSONField(verbose_name="Données envoyées à l'API")
+    status_code = models.IntegerField(verbose_name="code de retour de l'API")
+    response = models.TextField(verbose_name="réponse de l'API")
+    datas = models.JSONField(verbose_name="données envoyées à l'API")
     kind = models.CharField(
-        verbose_name="Type", choices=EmailSentTrackKind.choices, max_length=20, null=False, blank=False
+        verbose_name="type", choices=EmailSentTrackKind.choices, max_length=20, null=False, blank=False
     )
 
     class Meta:
-        verbose_name = "Trace des emails envoyés"
-        verbose_name_plural = "Traces des emails envoyés"
+        verbose_name = "trace des emails envoyés"
+        verbose_name_plural = "traces des emails envoyés"
 
     def __str__(self):
         return f"{self.status_code} - {self.created}"
 
 
 class BouncedEmail(DatedModel):
-    email = models.EmailField(verbose_name="Email", null=False, blank=False, unique=True)
-    reason = models.CharField(verbose_name="Raison", max_length=255, null=False, blank=False)
+    email = models.EmailField(verbose_name="email", null=False, blank=False, unique=True)
+    reason = models.CharField(verbose_name="raison", max_length=255, null=False, blank=False)
 
     class Meta:
-        verbose_name = "Emails en erreur"
-        verbose_name_plural = "Emails en erreur"
+        verbose_name = "email bloqué"
+        verbose_name_plural = "emails bloqués"
 
     def __str__(self):
         return f"{self.email} - {self.created}"
