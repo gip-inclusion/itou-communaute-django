13908c28a4443e5bb7c2b833e7b781071c2de391
vincent porte <vincent@neuralia.co>
2024-02-13 15:50:38 +0100

    (notification) delete BouncedEmail and BouncedDomainName

--- lacommunaute/notification/models.py
+++ lacommunaute/notification/models.py
@@ -18,27 +18,3 @@ class EmailSentTrack(DatedModel):
 
     def __str__(self):
         return f"{self.status_code} - {self.created}"
-
-
-class BouncedEmail(DatedModel):
-    email = models.EmailField(verbose_name="email", null=False, blank=False, unique=True)
-    reason = models.CharField(verbose_name="raison", max_length=255, null=False, blank=False)
-
-    class Meta:
-        verbose_name = "email bloqué"
-        verbose_name_plural = "emails bloqués"
-
-    def __str__(self):
-        return f"{self.email} - {self.created}"
-
-
-class BouncedDomainName(DatedModel):
-    domain = models.CharField(max_length=253, verbose_name="domaine", null=False, blank=False, unique=True)
-    reason = models.CharField(verbose_name="raison", max_length=255, null=False, blank=False)
-
-    class Meta:
-        verbose_name = "nom de domaine bloqué"
-        verbose_name_plural = "nom de domaines bloqués"
-
-    def __str__(self):
-        return f"{self.domain} - {self.created}"
--- lacommunaute/notification/tests/tests_models.py
+++ lacommunaute/notification/tests/tests_models.py
@@ -1,7 +1,5 @@
-from django.db import IntegrityError
 from django.test import TestCase
 
-from lacommunaute.notification.factories import BouncedDomainNameFactory, BouncedEmailFactory
 from lacommunaute.notification.models import EmailSentTrack
 
 
@@ -9,17 +7,3 @@ class EmailSentTrackModelTest(TestCase):
     def test_str(self):
         track = EmailSentTrack(status_code=200)
         self.assertEqual(str(track), f"200 - {track.created}")
-
-
-class BouncedEmailModelTest(TestCase):
-    def test_uniqueness(self):
-        bounced = BouncedEmailFactory()
-        with self.assertRaises(IntegrityError):
-            BouncedEmailFactory(email=bounced.email)
-
-
-class BouncedDomainNameModelTest(TestCase):
-    def test_uniqueness(self):
-        bounced = BouncedDomainNameFactory()
-        with self.assertRaises(IntegrityError):
-            BouncedDomainNameFactory(domain=bounced.domain)
