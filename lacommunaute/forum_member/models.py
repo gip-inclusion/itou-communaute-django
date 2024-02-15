787c1babde307e65de4dc4778e7257f21d81dc15
vincent porte <vincent@neuralia.co>
2024-02-15 14:13:31 +0100

    (forum profile) set ForumProfile str

--- lacommunaute/forum_member/models.py
+++ lacommunaute/forum_member/models.py
@@ -1,5 +1,8 @@
 from machina.apps.forum_member.abstract_models import AbstractForumProfile
 
+from lacommunaute.forum_member.shortcuts import get_forum_member_display_name
+
 
 class ForumProfile(AbstractForumProfile):
-    pass
+    def __str__(self):
+        return get_forum_member_display_name(self.user)
---
+++ lacommunaute/forum_member/tests/tests_models.py
@@ -0,0 +1,9 @@
+import pytest  # noqa
+
+from lacommunaute.forum_member.factories import ForumProfileFactory
+from lacommunaute.forum_member.shortcuts import get_forum_member_display_name
+
+
+def test_get_forum_member_display_name(db):
+    forum_profile = ForumProfileFactory()
+    assert forum_profile.__str__() == get_forum_member_display_name(forum_profile.user)
