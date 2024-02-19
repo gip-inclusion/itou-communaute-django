0569d2d07eb0e10ee2a41ce3d74ce174a0729be2
vincent porte <vincent@neuralia.co>
2024-02-19 19:16:44 +0100

    (moderation) let check_post_approbation render markdown version if post instance is not saved

--- lacommunaute/forum_moderation/utils.py
+++ lacommunaute/forum_moderation/utils.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from langdetect import detect
+from machina.models.fields import render_func
 from markdown2 import Markdown
 
 from lacommunaute.forum_moderation.models import BlockedDomainName, BlockedEmail
@@ -8,14 +9,17 @@ from lacommunaute.forum_moderation.models import BlockedDomainName, BlockedEmail
 def check_post_approbation(post):
     """
     Check if a post should be approved or not
+    Force markdown rendering to check for HTML tags
+    because the post is not save yet
     """
+    rendered = render_func(post.content.raw)
 
     conditions = [
         (
             post.username and BlockedDomainName.objects.filter(domain=post.username.split("@")[-1]).exists(),
             "Blocked Domain detected",
         ),
-        (Markdown.html_removed_text_compat in post.content.rendered, "HTML tags detected"),
+        (Markdown.html_removed_text_compat in rendered, "HTML tags detected"),
         (detect(post.content.raw) not in settings.LANGUAGE_CODE, "Alternative Language detected"),
         (post.username and BlockedEmail.objects.filter(email=post.username).exists(), "Blocked Email detected"),
     ]
