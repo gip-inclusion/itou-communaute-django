bef6ee88db9928022a8acbfcb1bef06d1f93e358
vincent porte <vincent@neuralia.co>
2024-03-11 18:48:52 +0100

    (surveys) add services fields (Dora's slug)

--- lacommunaute/surveys/admin.py
+++ lacommunaute/surveys/admin.py
@@ -11,4 +11,13 @@ class DSPAdmin(admin.ModelAdmin):
 
 @admin.register(Recommendation)
 class RecommendationAdmin(admin.ModelAdmin):
-    list_display = ("category", "text")
+    list_display = ("category", "text", "dora_cats", "dora_subs")
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
