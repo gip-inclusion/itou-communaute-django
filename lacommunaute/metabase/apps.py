bd9dd3266e2cf9d28f9c34b190e612771c95445c
vincent porte <vincent@neuralia.co>
2024-04-02 09:22:36 +0200

    (metabase): remove unused app, its models and codebase

--- lacommunaute/metabase/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class MetabaseAppConfig(AppConfig):
-    label = "metabase"
-    name = "lacommunaute.metabase"
-    verbose_name = "Metabase"
-    verbose_name_plural = "Metabase"
