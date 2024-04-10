dbcda903a3e98b18e7278d00971e9601e5f717f9
vincent porte <vincent@neuralia.co>
2024-04-10 15:47:23 +0200

    (perms) refactor the way to add public perms on a forum

--- lacommunaute/forum/factories.py
+++ lacommunaute/forum/factories.py
@@ -1,14 +1,10 @@
 import factory
-from machina.core.db.models import get_model
 from machina.test.factories.forum import ForumFactory as BaseForumFactory
 
 from lacommunaute.forum.models import Forum
 from lacommunaute.forum_upvote.models import UpVote
 from lacommunaute.users.factories import GroupFactory
-
-
-ForumPermission = get_model("forum_permission", "ForumPermission")
-UserForumPermission = get_model("forum_permission", "UserForumPermission")
+from lacommunaute.utils.perms import add_public_perms_on_forum
 
 
 class ForumFactory(BaseForumFactory):
@@ -26,30 +22,7 @@ class ForumFactory(BaseForumFactory):
         if not create or not extracted:
             return
 
-        perms = [
-            "can_see_forum",
-            "can_read_forum",
-            "can_start_new_topics",
-            "can_reply_to_topics",
-            "can_edit_own_posts",
-            "can_delete_own_posts",
-            "can_post_without_approval",
-        ]
-
-        anonymous_authorized_perms = [
-            UserForumPermission(
-                anonymous_user=True, authenticated_user=False, permission=permission, has_perm=True, forum=self
-            )
-            for permission in ForumPermission.objects.filter(codename__in=perms)
-        ]
-
-        authentified_authorized_perms = [
-            UserForumPermission(
-                anonymous_user=False, authenticated_user=True, permission=permission, has_perm=True, forum=self
-            )
-            for permission in ForumPermission.objects.filter(codename__in=perms)
-        ]
-        UserForumPermission.objects.bulk_create(anonymous_authorized_perms + authentified_authorized_perms)
+        add_public_perms_on_forum(self)
 
     @factory.post_generation
     def upvoted_by(self, create, extracted, **kwargs):
