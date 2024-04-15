efbbed610b56641b04f1d1e91057c2f872f0abbb
vincent porte <vincent@neuralia.co>
2024-04-15 14:00:04 +0200

    (forum) make forum_form more explicit

--- lacommunaute/forum/forms.py
+++ lacommunaute/forum/forms.py
@@ -4,6 +4,17 @@ from lacommunaute.forum.models import Forum
 
 
 class ForumForm(forms.ModelForm):
+    name = forms.CharField(required=True, label="Titre")
+    short_description = forms.CharField(
+        widget=forms.Textarea(attrs={"rows": 3}),
+        max_length=400,
+        required=True,
+        label="Sous-titre (400 caractères pour le SEO)",
+    )
+    description = forms.CharField(
+        widget=forms.Textarea(attrs={"rows": 20}), required=True, label="Contenu (markdown autorisé)"
+    )
+
     class Meta:
         model = Forum
         fields = ["name", "short_description", "description"]
