4e0cd588474adf17d8ad2501059b5c0c9b55cf3b
vincent porte <vincent@neuralia.co>
2024-04-15 09:46:26 +0200

    (forum) createview for top level category forum (documentation)

--- lacommunaute/forum/urls.py
+++ lacommunaute/forum/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from machina.apps.forum.views import IndexView
 
-from lacommunaute.forum.views import CategoryForumListView, ForumView
+from lacommunaute.forum.views import CategoryForumCreateView, CategoryForumListView, ForumView
 
 
 app_name = "forum_extension"
@@ -11,4 +11,5 @@ urlpatterns = [
     path("forum/<str:slug>-<int:pk>/", ForumView.as_view(), name="forum"),
     path("forums/", IndexView.as_view(), name="index"),
     path("documentation/", CategoryForumListView.as_view(), name="documentation"),
+    path("documentation/category/create/", CategoryForumCreateView.as_view(), name="create_category"),
 ]
