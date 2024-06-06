281d91d25b501985c763ba0f478c4c5075b51ded
vincent porte <vincent@neuralia.co>
2024-06-06 16:00:14 +0200

    fix(stats): rename MonthlyVisitorsView

--- lacommunaute/forum_stats/urls.py
+++ lacommunaute/forum_stats/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from lacommunaute.forum_stats.views import MonthlyVisitorsViews, StatistiquesPageView
+from lacommunaute.forum_stats.views import MonthlyVisitorsView, StatistiquesPageView
 
 
 app_name = "forum_stats"
 
 urlpatterns = [
     path("", StatistiquesPageView.as_view(), name="statistiques"),
-    path("monthly-visitors/", MonthlyVisitorsViews.as_view(), name="monthly_visitors"),
+    path("monthly-visitors/", MonthlyVisitorsView.as_view(), name="monthly_visitors"),
 ]
