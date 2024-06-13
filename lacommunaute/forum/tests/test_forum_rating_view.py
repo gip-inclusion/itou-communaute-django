6f140030f93a188fff9d0ffc88c6eab4ea89fa1a
vincent porte <vincent@neuralia.co>
2024-06-13 17:26:06 +0200

    feat(forum): add snapshot tests on ForumRatingViewe

--- lacommunaute/forum/tests/test_forum_rating_view.py
+++ lacommunaute/forum/tests/test_forum_rating_view.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from lacommunaute.forum.factories import ForumFactory
 from lacommunaute.forum.models import ForumRating
 from lacommunaute.users.factories import UserFactory
+from lacommunaute.utils.testing import parse_response_to_soup
 
 
 @pytest.fixture(name="public_forum")
@@ -16,7 +17,7 @@ def test_get_forum_rating_view(client, db, public_forum):
     assert response.status_code == 405
 
 
-def test_anonymous_post_forum_rating_view(client, db, public_forum):
+def test_anonymous_post_forum_rating_view(client, db, public_forum, snapshot):
     client.session.save()
 
     response = client.post(
@@ -26,6 +27,8 @@ def test_anonymous_post_forum_rating_view(client, db, public_forum):
     assert response.status_code == 200
     assert response.context["forum"] == public_forum
     assert response.context["rating"] == 5
+    content = parse_response_to_soup(response, replace_in_href=[public_forum])
+    assert str(content) == snapshot(name="anonymous_post_forum_rating_view")
 
     forum_rating = ForumRating.objects.get()
     assert forum_rating.forum == public_forum
@@ -34,7 +37,7 @@ def test_anonymous_post_forum_rating_view(client, db, public_forum):
     assert forum_rating.session_id == client.session.session_key
 
 
-def test_authenticated_post_forum_rating_view(client, db, public_forum):
+def test_authenticated_post_forum_rating_view(client, db, public_forum, snapshot):
     user = UserFactory()
     client.force_login(user)
 
@@ -43,6 +46,8 @@ def test_authenticated_post_forum_rating_view(client, db, public_forum):
         data={"rating": "1"},
     )
     assert response.status_code == 200
+    content = parse_response_to_soup(response, replace_in_href=[public_forum])
+    assert str(content) == snapshot(name="authenticated_post_forum_rating_view")
 
     forum_rating = ForumRating.objects.get()
     assert forum_rating.forum == public_forum
