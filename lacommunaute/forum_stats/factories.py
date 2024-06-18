8752857f9f82cfbf47e2f14239ec30ca3e8c1a96
Calum Mackervoy <calummackervoy@protonmail.com>
2024-06-18 10:38:53 +0200

    feat(forum_stats): SearchQueryFactory

--- lacommunaute/forum_stats/factories.py
+++ lacommunaute/forum_stats/factories.py
@@ -2,9 +2,10 @@ import datetime
 
 import factory
 import factory.django
+from django.conf import settings
 
 from lacommunaute.forum_stats.enums import Period
-from lacommunaute.forum_stats.models import Stat
+from lacommunaute.forum_stats.models import SearchCollectionPeriod, SearchQuery, Stat
 
 
 class StatFactory(factory.django.DjangoModelFactory):
@@ -23,3 +24,27 @@ class StatFactory(factory.django.DjangoModelFactory):
             value=46,
             period="day",
         )
+
+
+class SearchCollectionPeriodFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = SearchCollectionPeriod
+
+    name = factory.Faker("word")
+    start_date = factory.Faker("date")
+
+    @factory.lazy_attribute
+    def end_date(self):
+        return (
+            datetime.datetime.strptime(self.start_date, "%Y-%m-%d")
+            + datetime.timedelta(days=settings.SEARCH_COLLECTION_PERIOD_DAYS)
+        ).date()
+
+
+class SearchQueryFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = SearchQuery
+
+    label = factory.Faker("word")
+    period = factory.SubFactory(SearchCollectionPeriodFactory)
+    nb_visits = factory.Faker("random_int", min=1, max=10)
