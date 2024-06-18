015e08b9fc4fbc3f8f8e286f7c344746f13a3d4e
Vincent Porte <vincent@neuralia.co>
2024-06-18 12:01:12 +0200

    fix(stats): derniere date collectée des stats matomo (#684)
    ## Description
    
    🎸 Rechercher la derniere date uniquement sur les stats matomo
    
    ## Type de changement
    
    🪲 Correction de bug (changement non cassant qui corrige un problème).
    🚧 technique
    
    ### Points d'attention
    
    🦺 stats matomo : `["nb_uniq_visitors", "nb_uniq_visitors_returning",
    "nb_uniq_active_visitors", "nb_uniq_engaged_visitors"]`

--- lacommunaute/forum_stats/tests/tests_management_commands.py
+++ lacommunaute/forum_stats/tests/tests_management_commands.py
@@ -1,5 +1,5 @@
 import pytest  # noqa
-
+from lacommunaute.forum_stats.management.commands.collect_matomo_stats import get_initial_from_date
 from django.core.management import call_command
 from lacommunaute.surveys.factories import DSPFactory
 from lacommunaute.forum_stats.factories import StatFactory
@@ -11,3 +11,18 @@ def test_collect_django_stats(db, capsys):
     call_command("collect_django_stats")
     captured = capsys.readouterr()
     assert captured.out.strip() == "Collecting DSP stats from 2024-05-18 to yesterday: 1 new stats\nThat's all, folks!"
+
+
+@pytest.mark.parametrize(
+    "name", ["nb_uniq_visitors", "nb_uniq_visitors_returning", "nb_uniq_active_visitors", "nb_uniq_engaged_visitors"]
+)
+def test_get_initial_from_date_in_collect_matomo_stats(db, name):
+    # desired datas for sorting test
+    StatFactory(period="day", name=name, date="2024-05-17")
+    stat = StatFactory(period="day", name=name, date="2024-05-18")
+
+    # undesired datas
+    StatFactory(period="xxx", name=name, date="2024-05-19")
+    StatFactory(period="day", name="unexpected_name", date="2024-05-20")
+
+    assert get_initial_from_date("day") == stat
