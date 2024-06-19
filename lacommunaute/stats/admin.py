9027ae5b3e3255e0fd1ec67d61209b074cfd3618
vincent porte <vincent@neuralia.co>
2024-06-19 13:33:02 +0200

    refactor(stats): StatAdmin registering

--- lacommunaute/stats/admin.py
+++ lacommunaute/stats/admin.py
@@ -3,9 +3,7 @@ from django.contrib import admin
 from lacommunaute.stats.models import Stat
 
 
+@admin.register(Stat)
 class StatAdmin(admin.ModelAdmin):
     list_display = ("name", "date", "value", "period")
     list_filter = ("name", "date", "period")
-
-
-admin.site.register(Stat, StatAdmin)
