5106db73374b7a4850a901a398da53cc64b1964b
vincent porte <vincent@neuralia.co>
2024-06-27 17:22:12 +0200

    update homepage

--- lacommunaute/pages/tests/test_homepage.py
+++ lacommunaute/pages/tests/test_homepage.py
@@ -4,15 +4,18 @@ from django.urls import reverse
 from django.utils import timezone
 from pytest_django.asserts import assertContains, assertNotContains
 
+from lacommunaute.edito.factories import EditoFactory
 from lacommunaute.event.factories import EventFactory
 from lacommunaute.forum.enums import Kind as ForumKind
 from lacommunaute.forum.factories import ForumFactory
 from lacommunaute.forum_conversation.factories import PostFactory, TopicFactory
+from lacommunaute.utils.testing import parse_response_to_soup
 
 
 def test_context_data(client, db):
     topic = TopicFactory(with_post=True, forum=ForumFactory())
     article = ForumFactory(parent=ForumFactory(type=1))
+    edito = EditoFactory()
 
     disapproved_topic = TopicFactory(with_post=True, forum=ForumFactory())
     disapproved_topic.approved = False
@@ -26,6 +29,7 @@ def test_context_data(client, db):
 
     assert response.context_data["topics_public"].get() == topic
     assert response.context_data["forums_category"].get() == article
+    assert response.context_data["edito"] == edito
 
 
 def test_new_topics_order(client, db):
@@ -61,3 +65,14 @@ def test_events(db, client):
         assertContains(response, reverse("event:detail", kwargs={"pk": future_event.pk}))
     assertNotContains(response, unvisible_future_event.name)
     assertContains(response, reverse("event:current"))
+
+
+def test_edito(db, client, snapshot):
+    EditoFactory()
+    EditoFactory(for_snapshot=True)
+
+    url = reverse("pages:home")
+
+    response = client.get(url)
+    assert response.status_code == 200
+    assert str(parse_response_to_soup(response, selector=".s-section__container")) == snapshot(name="edito")
