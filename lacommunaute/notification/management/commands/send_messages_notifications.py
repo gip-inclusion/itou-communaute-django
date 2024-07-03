32f59ed0a71a217393aded9febbea04f1d59e5ee
Calum Mackervoy <calummackervoy@protonmail.com>
2024-07-03 17:31:58 +0200

    fix(cron.json): erreur copier-coller sur send_notifications_daily.sh (#705)
    ## Description
    
    🎸 La tâche quotidien utilisait la fausse commande
    🎸 Scriptes shells n'étaient pas executables
    
    ## Type de changement
    
    🪲 Correction de bug (changement non cassant qui corrige un problème).

--- lacommunaute/notification/management/commands/send_messages_notifications.py
+++ lacommunaute/notification/management/commands/send_messages_notifications.py
@@ -7,10 +7,10 @@ from lacommunaute.notification.tasks import send_messages_notifications
 class Command(BaseCommand):
     help = "Envoyer les notifications en file d'attente avec le délai paramétré"
 
-    def handle(self, *args, **options):
-        def add_arguments(self, parser):
-            parser.add_argument("delay", type=str, help=f"le délai, un valeur de {str(NotificationDelay.values)}")
+    def add_arguments(self, parser):
+        parser.add_argument("delay", type=str, help=f"le délai, un valeur de {str(NotificationDelay.values)}")
 
+    def handle(self, *args, **options):
         try:
             delay = NotificationDelay(options["delay"])
         except ValueError:
