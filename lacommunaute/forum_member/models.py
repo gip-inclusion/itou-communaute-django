4493267ec9f3919acb91c31db18f78c7c4ac074f
Vincent Porte <vincent@neuralia.co>
2024-08-07 13:08:06 +0200

    feat: mise à jour entête, pied de page et menu (#739)
    ## Description
    
    🎸 mise à jour menu
    🎸 mise à jour header
    🎸 mise à jour footer
    
    ## Type de changement
    
    🎨 changement d'UI
    
    ### Captures d'écran (optionnel)
    
    Header large
    
    
    ![image](https://github.com/user-attachments/assets/2c694118-418e-43ab-8c60-0dc8e0851535)
    
    
    Header large authentifié
    
    
    ![image](https://github.com/user-attachments/assets/eda3bb68-11b9-4b33-8fb0-01daca2a756f)
    
    
    Header medium
    
    
    ![image](https://github.com/user-attachments/assets/4606a875-f92c-4d85-8ef9-21ae6eb10b9d)
    
    Header small
    
    
    ![image](https://github.com/user-attachments/assets/5207b69e-8e22-4a33-93a8-469c779e2989)
    
    Menu anonyme
    
    
    ![image](https://github.com/user-attachments/assets/92d1a0a1-2f2e-4b8b-b114-2c001bd0016a)
    
    Menu authentifié
    
    
    ![image](https://github.com/user-attachments/assets/0e409c48-0325-4a53-b9dc-bb82fe2a49a7)
    
    
    
    Footer large 
    
    
    ![image](https://github.com/user-attachments/assets/cd7faf27-1e86-4c0f-8c2c-d4926d8e75e1)
    
    ---------
    
    Co-authored-by: hellodeloo <hello@hellodeloo.com>

--- lacommunaute/forum_member/models.py
+++ lacommunaute/forum_member/models.py
@@ -1,22 +1,8 @@
-from dateutil.relativedelta import relativedelta
 from django.db import models
-from django.utils import timezone
 from machina.apps.forum_member.abstract_models import AbstractForumProfile
 
 from lacommunaute.forum_member.enums import ActiveSearch, Regions
 from lacommunaute.forum_member.shortcuts import get_forum_member_display_name
-
-
-class ForumProfileQuerySet(models.QuerySet):
-    def power_users(self):
-        return (
-            self.filter(user__posts__created__gte=timezone.now() - relativedelta(days=30))
-            .annotate(user_posts_count=models.Count("user__posts"))
-            .filter(user_posts_count__gte=3)
-            .order_by("-user_posts_count")
-            .select_related("user")
-            .prefetch_related("user__posts")
-        )
 
 
 class ForumProfile(AbstractForumProfile):
@@ -35,8 +21,6 @@ class ForumProfile(AbstractForumProfile):
     )
     internship_duration = models.IntegerField(default=0, verbose_name="durée du stage (en mois)")
     updated_at = models.DateTimeField(auto_now=True)
-
-    objects = ForumProfileQuerySet().as_manager()
 
     def __str__(self):
         return get_forum_member_display_name(self.user)
--- lacommunaute/forum_member/tests/tests_models.py
+++ lacommunaute/forum_member/tests/tests_models.py
@@ -1,6 +1,4 @@
 import pytest  # noqa
-from dateutil.relativedelta import relativedelta
-from django.utils import timezone
 
 from lacommunaute.forum_conversation.factories import (  # noqa
     AnonymousPostFactory,
@@ -9,45 +7,9 @@ from lacommunaute.forum_conversation.factories import (  # noqa
     TopicFactory,
 )
 from lacommunaute.forum_member.factories import ForumProfileFactory
-from lacommunaute.forum_member.models import ForumProfile
 from lacommunaute.forum_member.shortcuts import get_forum_member_display_name
 
 
 def test_get_forum_member_display_name(db):
     forum_profile = ForumProfileFactory()
     assert forum_profile.__str__() == get_forum_member_display_name(forum_profile.user)
-
-
-def test_powerusers_forumprofile_queryset(db):
-    # anonymous user with 3 posts within the last 30 days
-    topic1 = AnonymousTopicFactory(with_post=True)
-    AnonymousPostFactory.create_batch(2, topic=topic1, username=topic1.first_post.username)
-
-    # authenticated user with 2 posts within the last 30 days
-    forum_profile1 = ForumProfileFactory()
-    topic2 = TopicFactory(with_post=True, poster=forum_profile1.user)
-    PostFactory(topic=topic2, poster=forum_profile1.user)
-
-    # authenticated user with 3 post before the last 30 days
-    forum_profile2 = ForumProfileFactory()
-    topic3 = TopicFactory(with_post=True, poster=forum_profile2.user)
-    PostFactory(topic=topic3, poster=forum_profile2.user)
-    for post in topic3.posts.all():
-        post.created = timezone.now() - relativedelta(days=31)
-        post.save()
-
-    assert ForumProfile.objects.power_users().count() == 0
-
-    # authenticated user with 3 posts within the last 30 days
-    poweruser = ForumProfileFactory()
-    topic4 = TopicFactory(with_post=True, poster=poweruser.user)
-    PostFactory.create_batch(2, topic=topic4, poster=poweruser.user)
-
-    # authenticated user with more than 3 posts within the last 30 days
-    bigpoweruser = ForumProfileFactory()
-    topic5 = TopicFactory(with_post=True, poster=bigpoweruser.user)
-    PostFactory.create_batch(20, topic=topic5, poster=bigpoweruser.user)
-
-    assert bigpoweruser == ForumProfile.objects.power_users().first()
-    assert poweruser == ForumProfile.objects.power_users().last()
-    assert ForumProfile.objects.power_users().count() == 2
