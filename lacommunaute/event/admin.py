ca56d83be47fcf8c628c19fd141ecf73a4adeadc
Vincent Porte <vincent@neuralia.co>
2024-08-14 11:02:53 +0200

    feat(event): amélioration de l'admin (#753)
    ## Description
    
    🎸 Ajout de filtres, de tris et du champ de recherche dans l'interface
    admin des `Event`
    
    ## Type de changement
    
    🚧 technique
    
    ### Captures d'écran (optionnel)
    
    
    ![image](https://github.com/user-attachments/assets/42917163-2f8f-4203-bb9b-4f4d1cd2005a)

--- lacommunaute/event/admin.py
+++ lacommunaute/event/admin.py
@@ -5,6 +5,9 @@ from lacommunaute.event.models import Event
 
 class EventAdmin(admin.ModelAdmin):
     raw_id_fields = ("poster",)
+    list_display = ("name", "date", "poster")
+    list_filter = ("date",)
+    search_fields = ("name", "poster__email")
 
 
 admin.site.register(Event, EventAdmin)
