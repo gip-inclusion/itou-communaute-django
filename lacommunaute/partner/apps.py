b76573e5246e2276d0c936c5c937bf2a89aeb37c
vincent porte <vincent@neuralia.co>
2024-08-28 14:35:43 +0200

    feat(partner): add app

---
+++ lacommunaute/partner/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class PartenerAppConfig(AppConfig):
+    label = "partner"
+    name = "lacommunaute.partner"
+    verbose_name = "Partner"
+    verbose_name_plural = "Partners"
