97879663a3084cc6b396c66d0a1be916d9540329
vincent porte <vincent@neuralia.co>
2024-09-03 18:01:07 +0200

    fixup! let choose a partner in forum update view

--- lacommunaute/forum/tests/tests_forms.py
+++ lacommunaute/forum/tests/tests_forms.py
@@ -42,12 +42,12 @@ def test_saved_forum_description(db):
 def test_form_field():
     form = ForumForm()
     assert form.Meta.model == Forum
-    assert form.Meta.fields == ["name", "short_description", "description", "image", "certified"]
+    assert form.Meta.fields == ["name", "short_description", "description", "image", "certified", "partner"]
     assert form.fields["name"].required
     assert form.fields["short_description"].required
     assert not form.fields["description"].required
     assert not form.fields["image"].required
     assert not form.fields["certified"].required
+    assert not form.fields["partner"].required
     assert not form.fields["tags"].required
     assert not form.fields["new_tags"].required
-    assert not form.fields["partner"].required
