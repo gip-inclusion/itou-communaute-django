810e2d4c490489e71e4adf83982ed4e4ba85ee27
vincent porte <vincent@neuralia.co>
2024-09-03 16:55:58 +0200

    add linked forums to partner detailview

--- lacommunaute/partner/views.py
+++ lacommunaute/partner/views.py
@@ -1,6 +1,11 @@
 from django.views.generic import DetailView, ListView
+from machina.core.loading import get_class
 
+from lacommunaute.forum.models import Forum
 from lacommunaute.partner.models import Partner
+
+
+ForumVisibilityContentTree = get_class("forum.visibility", "ForumVisibilityContentTree")
 
 
 class PartnerListView(ListView):
@@ -14,3 +19,13 @@ class PartnerDetailView(DetailView):
     model = Partner
     template_name = "partner/detail.html"
     context_object_name = "partner"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["sub_forums"] = ForumVisibilityContentTree.from_forums(
+            self.request.forum_permission_handler.forum_list_filter(
+                Forum.objects.filter(partner=self.object),
+                self.request.user,
+            ),
+        )
+        return context
