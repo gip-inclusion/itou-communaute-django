9b3086da82d08b6ea9fcfef421fedd2a4f41cbbf
vincent porte <vincent@neuralia.co>
2024-09-03 15:18:41 +0200

    add Forum inlines in Partner admin

--- lacommunaute/partner/admin.py
+++ lacommunaute/partner/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 
+from lacommunaute.forum.models import Forum
 from lacommunaute.partner.models import Partner
+
+
+class ForumInline(admin.TabularInline):
+    model = Forum
+    extra = 0
+    fields = ("name", "short_description")
+    readonly_fields = ("name", "short_description")
+
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 
 @admin.register(Partner)
@@ -8,3 +19,4 @@ class PartnerAdmin(admin.ModelAdmin):
     list_display = ("name", "url", "created")
     search_fields = ("name",)
     list_filter = ("created",)
+    inlines = [ForumInline]
