a0a4cb104f797939782faca97ece3c8ab30742bd
vincent porte <vincent@neuralia.co>
2024-09-23 12:19:57 +0200

    add link to PartnerCreateView in PartnerListView for superuser

--- lacommunaute/partner/tests/tests_partner_listview.py
+++ lacommunaute/partner/tests/tests_partner_listview.py
@@ -2,6 +2,7 @@ import pytest  # noqa
 from django.urls import reverse
 
 from lacommunaute.partner.factories import PartnerFactory
+from lacommunaute.users.factories import UserFactory
 from lacommunaute.utils.testing import parse_response_to_soup
 
 
@@ -24,3 +25,15 @@ def test_pagination(client, db, snapshot, url):
     response = client.get(url)
     assert response.status_code == 200
     assert str(parse_response_to_soup(response, selector="ul.pagination")) == snapshot(name="partner_pagination")
+
+
+@pytest.mark.parametrize(
+    "user,link_is_visible",
+    [(None, False), (lambda: UserFactory(), False), (lambda: UserFactory(is_superuser=True), True)],
+)
+def test_link_to_createview(client, db, url, user, link_is_visible):
+    if user:
+        client.force_login(user())
+    response = client.get(url)
+    assert response.status_code == 200
+    assert bool(reverse("partner:create") in response.content.decode()) == link_is_visible
