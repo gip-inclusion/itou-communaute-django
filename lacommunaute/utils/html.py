b790732d3fe1d7817156946efd5774970ea8aac0
vincent porte <vincent@neuralia.co>
2024-09-23 12:10:49 +0200

    improve regex efficiency

--- lacommunaute/utils/html.py
+++ lacommunaute/utils/html.py
@@ -5,7 +5,7 @@ def wrap_iframe_in_div_tag(text):
     # iframe tags must be wrapped in a div tag to be displayed correctly
     # add div tag if not present
 
-    iframe_regex = r"((<div>)?<iframe.*?</iframe>(</div>)?)"
+    iframe_regex = r"((<div>)?<iframe[^>]*>.*?<\/iframe>(<\/div>)?)"
 
     for match, starts_with, ends_with in re.findall(iframe_regex, text, re.DOTALL):
         if not starts_with and not ends_with:
