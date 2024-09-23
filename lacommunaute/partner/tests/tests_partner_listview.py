db7be24b456fc8281a8c0fe6c74afb2aa27f5a2b
vincent porte <vincent@neuralia.co>
2024-09-23 12:16:19 +0200

    use url fixture in PartnerListView tests

--- lacommunaute/partner/tests/tests_partner_listview.py
+++ lacommunaute/partner/tests/tests_partner_listview.py
@@ -5,17 +5,22 @@ from lacommunaute.partner.factories import PartnerFactory
 from lacommunaute.utils.testing import parse_response_to_soup
 
 
-def test_listview(client, db, snapshot):
+@pytest.fixture(name="url")
+def url_fixture():
+    return reverse("partner:list")
+
+
+def test_listview(client, db, snapshot, url):
     partner = PartnerFactory(for_snapshot=True, with_logo=True)
-    response = client.get(reverse("partner:list"))
+    response = client.get(url)
     assert response.status_code == 200
     assert str(
         parse_response_to_soup(response, selector="#partner-list", replace_img_src=True, replace_in_href=[partner])
     ) == snapshot(name="partner_listview")
 
 
-def test_pagination(client, db, snapshot):
+def test_pagination(client, db, snapshot, url):
     PartnerFactory.create_batch(8 * 3 + 1)
-    response = client.get(reverse("partner:list"))
+    response = client.get(url)
     assert response.status_code == 200
     assert str(parse_response_to_soup(response, selector="ul.pagination")) == snapshot(name="partner_pagination")
