b9ccbc973ca036fc2c3e10aec5d502b1fe17570e
vincent porte <vincent@neuralia.co>
2024-09-23 17:52:50 +0200

    setup documentation app

---
+++ lacommunaute/documentation/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class ForumUpVoteAppConfig(AppConfig):
+    label = "documentation"
+    name = "lacommunaute.documentation"
+    verbose_name = "Documentation"
+    verbose_name_plural = "Documentation"
