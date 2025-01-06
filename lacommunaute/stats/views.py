9da31ff1bc239d6fb9b984bedb8982cffc952162
vincent porte <vincent@neuralia.co>
2025-01-06 16:09:32 +0100

    remove deactivation messages in stats pages

--- lacommunaute/stats/views.py
+++ lacommunaute/stats/views.py
@@ -1,7 +1,6 @@
 import logging
 
 from dateutil.relativedelta import relativedelta
-from django.contrib import messages
 from django.db.models import Avg, CharField, Count, OuterRef, Subquery, Sum
 from django.db.models.functions import Cast
 from django.shortcuts import render
@@ -80,8 +79,6 @@ class StatistiquesPageView(TemplateView):
         context["dsp_count"] = self.get_dsp_count()
         context = {**context, **self.get_funnel_data()}
 
-        messages.warning(self.request, "La collecte des statistiques est desactivée pour le moment.")
-
         return context
 
 
@@ -102,8 +99,6 @@ class BaseDetailStatsView(TemplateView):
         context = super().get_context_data(**kwargs)
         context["stats"] = self.get_detailled_stats()
         context["box_title"] = self.box_title
-
-        messages.warning(self.request, "La collecte des statistiques est desactivée pour le moment.")
 
         return context
 
@@ -161,8 +156,6 @@ class DocumentStatsView(View):
             else "sum_time_spent"
         )
         objects = objects.order_by("-" + sort_key)
-
-        messages.warning(self.request, "La collecte des statistiques est desactivée pour le moment.")
 
         return render(
             request,
