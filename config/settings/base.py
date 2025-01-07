b98e83f4e16754ea9aa2099183624efe69c6de8b
vincent porte <vincent@neuralia.co>
2025-01-07 08:06:45 +0100

    remove default value for ENVIRONMENT in base.py because it must defined as env vars in virtual server

--- config/settings/base.py
+++ config/settings/base.py
@@ -3,8 +3,6 @@ import os
 from dotenv import load_dotenv
 from machina import MACHINA_MAIN_STATIC_DIR, MACHINA_MAIN_TEMPLATE_DIR
 
-from lacommunaute.utils.enums import Environment
-
 
 load_dotenv()
 
@@ -19,7 +17,7 @@ APPS_DIR = os.path.abspath(os.path.join(ROOT_DIR, "lacommunaute"))
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 DEBUG = os.getenv("DJANGO_DEBUG", "False") == "True"
-ENVIRONMENT = os.getenv("ENVIRONMENT", Environment.PROD)
+ENVIRONMENT = os.getenv("ENVIRONMENT")
 
 PARKING_PAGE = os.getenv("PARKING_PAGE", "False") == "True"
 
