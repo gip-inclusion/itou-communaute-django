3bea003d4feca336d89ae5b6ce7f626434fe528e
vincent porte <vincent@neuralia.co>
2025-01-09 16:05:18 +0100

    generate requirements when deploying

--- clevercloud/pre_build.sh
+++ clevercloud/pre_build.sh
@@ -1,2 +1,5 @@
 git clone "$ITOU_SECRETS_HTTPS_REPO_URL" secrets-vault
 sops -d secrets-vault/c3/"$ENVIRONMENT".enc.env > .env
+
+# generate requirements file to let clevercloud know which packages to install
+uv export --format requirements-txt --no-dev --frozen > requirements.txt
