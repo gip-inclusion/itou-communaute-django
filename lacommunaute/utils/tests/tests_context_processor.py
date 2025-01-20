84d916828368deb4cbaab4cc6f44bdae48a15f9b
Vincent Porte <vincentporte@duck.com>
2025-01-20 10:53:33 +0100

    chore: afficher la bannière d'environnement systématiquement hors PROD (#881)
    ## Description
    
    🎸 cf title
    
    ## Type de changement
    
    🎨 changement d'UI
    🚧 technique
    
    
    
    ### Captures d'écran (optionnel)
    
    dev local
    
    ![image](https://github.com/user-attachments/assets/72a1c8c4-5c92-4461-bf1f-bb8911cf4158)
    
    review app
    
    ![image](https://github.com/user-attachments/assets/705067f1-7909-45b5-a2cd-db7f222d25c9)

--- lacommunaute/utils/tests/tests_context_processor.py
+++ lacommunaute/utils/tests/tests_context_processor.py
@@ -2,20 +2,25 @@ import pytest
 from django.test import override_settings
 
 from lacommunaute.utils.enums import Environment
+from lacommunaute.utils.testing import parse_response_to_soup
 
 
 @pytest.mark.parametrize(
     "env,expected",
     [
+        (None, False),
         (Environment.PROD, False),
-        (Environment.TEST, False),
+        (Environment.TEST, True),
         (Environment.DEV, True),
     ],
 )
-def test_prod_environment(client, db, env, expected):
+def test_prod_environment(client, db, env, expected, snapshot):
     with override_settings(ENVIRONMENT=env):
         response = client.get("/")
     assert ('id="debug-mode-banner"' in response.content.decode()) == expected
+    if expected:
+        content = parse_response_to_soup(response, selector="#debug-mode-banner")
+        assert str(content) == snapshot(name=env.label)
 
 
 def test_exposed_settings(client, db):
