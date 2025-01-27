bca414a26a48b6ceca0622e0fb80bc0bc95d5b38
vincent porte <vincent@neuralia.co>
2025-01-27 17:28:01 +0100

    refactor Upvote model tests in pytest style

--- lacommunaute/forum_upvote/tests/tests_models.py
+++ lacommunaute/forum_upvote/tests/tests_models.py
@@ -1,28 +1,28 @@
+import pytest
 from django.db import IntegrityError
-from django.test import TestCase
 
 from lacommunaute.forum_conversation.factories import TopicFactory
 from lacommunaute.forum_upvote.models import UpVote
 
 
-class UpVoteModelTest(TestCase):
-    def test_generic_relation(self):
+class TestUpVoteModel:
+    def test_generic_relation(self, db):
         topic = TopicFactory(with_post=True)
         UpVote.objects.create(content_object=topic.first_post, voter=topic.first_post.poster)
         UpVote.objects.create(content_object=topic.forum, voter=topic.first_post.poster)
 
-        self.assertEqual(UpVote.objects.count(), 2)
+        assert UpVote.objects.count() == 2
 
-    def test_upvoted_post_unicity(self):
+    def test_upvoted_post_unicity(self, db):
         topic = TopicFactory(with_post=True)
         UpVote.objects.create(content_object=topic.first_post, voter=topic.first_post.poster)
 
-        with self.assertRaises(IntegrityError):
+        with pytest.raises(IntegrityError):
             UpVote.objects.create(content_object=topic.first_post, voter=topic.first_post.poster)
 
-    def test_upvoted_forum_unicity(self):
+    def test_upvoted_forum_unicity(self, db):
         topic = TopicFactory(with_post=True)
         UpVote.objects.create(content_object=topic.forum, voter=topic.first_post.poster)
 
-        with self.assertRaises(IntegrityError):
+        with pytest.raises(IntegrityError):
             UpVote.objects.create(content_object=topic.forum, voter=topic.first_post.poster)
