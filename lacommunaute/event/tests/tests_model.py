727f5e63a9eb1a44676f97ec6bee5f3cf7bddb99
vincent porte <vincent@neuralia.co>
2025-01-27 16:43:41 +0100

    update EmailLastSeen when saving Event

--- lacommunaute/event/tests/tests_model.py
+++ lacommunaute/event/tests/tests_model.py
@@ -2,9 +2,18 @@ from django.db import IntegrityError
 from django.test import TestCase
 
 from lacommunaute.event.factories import EventFactory
+from lacommunaute.users.models import EmailLastSeen
 
 
 class EventModelTest(TestCase):
     def test_user_is_mandatory(self):
         with self.assertRaises(IntegrityError):
             EventFactory(poster=None)
+
+
+class TestEventModel:
+    def test_email_last_seen_updated_on_save(self, db):
+        EventFactory()
+
+        email_last_seen = EmailLastSeen.objects.get()
+        assert email_last_seen.last_seen_kind == "EVENT"
