67a62f8e47f13f44212fe4792875802a97a9bcbb
vincent porte <vincent@neuralia.co>
2025-02-05 11:54:18 +0100

    refactor User model test

--- lacommunaute/users/tests/tests_models.py
+++ lacommunaute/users/tests/tests_models.py
@@ -3,7 +3,11 @@ import re
 from lacommunaute.users.models import User
 
 
-def test_create_user_without_username(db):
-    user = User.objects.create_user(email="alex@honnold.com")
-    assert re.match(r"^[a-f0-9]{8}(-[a-f0-9]{4}){3}-[a-f0-9]{12}$", user.username)
-    assert user.email == "alex@honnold.com"
+EMAIL = "alex@honnold.com"
+
+
+class TestUserModel:
+    def test_create_user_without_username(self, db):
+        user = User.objects.create_user(email=EMAIL)
+        assert re.match(r"^[a-f0-9]{8}(-[a-f0-9]{4}){3}-[a-f0-9]{12}$", user.username)
+        assert user.email == EMAIL
