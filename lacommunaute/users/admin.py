f2b6ed91c03dfbc1f80b9524882ce88e4e493dfe
vincent porte <vincent@neuralia.co>
2025-02-05 11:35:09 +0100

    fix User model import in admin

--- lacommunaute/users/admin.py
+++ lacommunaute/users/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import Group
 
-from .models import User
+from lacommunaute.users.models import User
 
 
 class UserAdmin(UserAdmin):
