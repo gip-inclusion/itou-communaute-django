95e23a750bd0035a17a4164cfc420ccd9ea8ae57
vincent porte <vincent@neuralia.co>
2025-02-12 16:45:08 +0100

    add send_missyou_notifications tasks

--- lacommunaute/notification/management/commands/send_messages_notifications.py
+++ lacommunaute/notification/management/commands/send_messages_notifications.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand
 
 from lacommunaute.notification.enums import NotificationDelay
-from lacommunaute.notification.tasks import send_messages_notifications
+from lacommunaute.notification.tasks import send_messages_notifications, send_missyou_notifications
 
 
 class Command(BaseCommand):
@@ -19,4 +19,5 @@ class Command(BaseCommand):
             )
 
         send_messages_notifications(delay)
+        send_missyou_notifications(delay)
         self.stdout.write(self.style.SUCCESS("That's all, folks!"))
