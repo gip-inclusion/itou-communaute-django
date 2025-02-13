8e39373e8d71847b43f3ebe6695a19e9db6666a4
vincent porte <vincent@neuralia.co>
2025-02-13 14:56:56 +0100

    add staff perms when creating a new forum

--- lacommunaute/forum/tests/test_categoryforum_createview.py
+++ lacommunaute/forum/tests/test_categoryforum_createview.py
@@ -1,12 +1,20 @@
+import pytest
+from django.conf import settings
 from django.urls import reverse
 from machina.core.db.models import get_model
 from pytest_django.asserts import assertContains
 
 from lacommunaute.forum.models import Forum
-from lacommunaute.users.factories import UserFactory
+from lacommunaute.users.factories import GroupFactory, UserFactory
 
 
 UserForumPermission = get_model("forum_permission", "UserForumPermission")
+GroupForumPermission = get_model("forum_permission", "GroupForumPermission")
+
+
+@pytest.fixture(name="staff_group")
+def fixture_staff_group():
+    return GroupFactory(id=settings.STAFF_GROUP_ID)
 
 
 def test_user_access(client, db):
@@ -33,7 +41,7 @@ def test_form_title_and_context_data(client, db):
     assertContains(response, reverse("forum_extension:documentation"))
 
 
-def test_success_url(client, db):
+def test_success_url(client, db, staff_group):
     client.force_login(UserFactory(is_staff=True))
     url = reverse("forum_extension:create_category")
     response = client.post(url, data={"name": "Test", "description": "Test", "short_description": "Test"})
@@ -41,7 +49,7 @@ def test_success_url(client, db):
     assert response.url == reverse("forum_extension:documentation")
 
 
-def test_create_category_with_perms(client, db):
+def test_create_category_with_perms(client, db, staff_group):
     client.force_login(UserFactory(is_staff=True))
     url = reverse("forum_extension:create_category")
     response = client.post(url, data={"name": "Test", "description": "Test", "short_description": "Test"})
@@ -52,3 +60,4 @@ def test_create_category_with_perms(client, db):
     assert forum.parent is None
 
     assert UserForumPermission.objects.filter(forum=forum).count() == 14
+    assert GroupForumPermission.objects.filter(forum=forum, group=staff_group).count() == 3
--- lacommunaute/forum/tests/test_subcategoryforum_createview.py
+++ lacommunaute/forum/tests/test_subcategoryforum_createview.py
@@ -1,13 +1,21 @@
+import pytest
+from django.conf import settings
 from django.urls import reverse
 from machina.core.db.models import get_model
 from pytest_django.asserts import assertContains
 
 from lacommunaute.forum.factories import CategoryForumFactory
 from lacommunaute.forum.models import Forum
-from lacommunaute.users.factories import UserFactory
+from lacommunaute.users.factories import GroupFactory, UserFactory
 
 
 UserForumPermission = get_model("forum_permission", "UserForumPermission")
+GroupForumPermission = get_model("forum_permission", "GroupForumPermission")
+
+
+@pytest.fixture(name="staff_group")
+def fixture_staff_group():
+    return GroupFactory(id=settings.STAFF_GROUP_ID)
 
 
 def test_user_access(client, db):
@@ -38,7 +46,7 @@ def test_form_title_and_context_datas(client, db):
     )
 
 
-def test_success_url(client, db):
+def test_success_url(client, db, staff_group):
     client.force_login(UserFactory(is_staff=True))
     category_forum = CategoryForumFactory()
     url = reverse("forum_extension:create_subcategory", kwargs={"pk": category_forum.pk})
@@ -49,7 +57,7 @@ def test_success_url(client, db):
     )
 
 
-def test_create_subcategory_with_perms(client, db):
+def test_create_subcategory_with_perms(client, db, staff_group):
     client.force_login(UserFactory(is_staff=True))
     category_forum = CategoryForumFactory()
     url = reverse("forum_extension:create_subcategory", kwargs={"pk": category_forum.pk})
@@ -61,3 +69,4 @@ def test_create_subcategory_with_perms(client, db):
     assert forum.parent == category_forum
 
     assert UserForumPermission.objects.filter(forum=forum).count() == 14
+    assert GroupForumPermission.objects.filter(forum=forum, group=staff_group).count() == 3
