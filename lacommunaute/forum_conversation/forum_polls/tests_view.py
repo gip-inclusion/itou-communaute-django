2186be1ba4acbaf4e5566b0d5d6e71b93f922ffc
vincent porte <vincent@neuralia.co>
2025-03-14 11:02:06 +0100

    use ForumFactory called by TopicFactory

--- lacommunaute/forum_conversation/forum_polls/tests_view.py
+++ lacommunaute/forum_conversation/forum_polls/tests_view.py
@@ -2,7 +2,6 @@ from django.test import TestCase
 from django.urls import reverse
 from machina.core.db.models import get_model
 from machina.core.loading import get_class
-from machina.test.factories.forum import create_forum
 from machina.test.factories.polls import TopicPollFactory, TopicPollOptionFactory
 
 from lacommunaute.forum_conversation.factories import PostFactory, TopicFactory
@@ -21,8 +20,8 @@ class TopicPollVoteViewTest(TestCase):
     def setUpTestData(cls):
         cls.user = UserFactory()
         cls.perm_handler = PermissionHandler()
-        cls.forum = create_forum()
-        cls.topic = TopicFactory(forum=cls.forum, poster=cls.user)
+        cls.topic = TopicFactory(poster=cls.user)
+        cls.forum = cls.topic.forum
         cls.post = PostFactory.create(topic=cls.topic, poster=cls.user)
         cls.poll_option = TopicPollOptionFactory(poll=TopicPollFactory(topic=cls.topic))
         cls.url = reverse(
