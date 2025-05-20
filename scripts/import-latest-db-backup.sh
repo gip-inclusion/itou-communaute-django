feda1d0b66a2699fb4929a416dfd109513a09426
Céline Martinet Sanchez <celine.martinet.sanchez+github@gmail.com>
2025-05-20 16:57:58 +0200

    Improve import-latest-db-backups
    - Add an `rclone` command to download the latest available backup.
    - Add the possibility to change the default target database name.
    - Remove backup file at script finishing.

--- scripts/import-latest-db-backup.sh
+++ scripts/import-latest-db-backup.sh
@@ -3,33 +3,35 @@
 # PLEASE RUN THIS SCRIPT WITH:
 # make postgres_restore_latest_backup
 
-source .env
+source .envrc
 
-if [ -z $PATH_TO_BACKUPS ]; then
-  echo "please add 'PATH_TO_BACKUPS=/your/backups/directory' in .env at the root of the project in order to run this script"
-  exit
+# Restore the latest database dump kept on Scaleway to the local database.
+# Default database is $PGDATABASE but you can add a positional argument to change it.
+# For example: `./scripts/import-latest-db-backup.sh communaute-real-world
+# Make sure you installed the `itou-backups` project before. Also, check that a database server is running.
+# $PATH_TO_BACKUPS is mandatory.
+
+if [ ! -f "$PATH_TO_BACKUPS/.env" ]; then
+    echo "Backups .env file not found. Stopping here."
+    exit 0
 fi
 
-# Download last available backup
-# rclone copy --max-age 24h --progress communaute:/encrypted-backups ./backups
-echo "please rclone the last db backup to $PATH_TO_BACKUPS"
+# https://www.shellcheck.net/wiki/SC1091
+# shellcheck disable=SC1091
+source "$PATH_TO_BACKUPS/.env"
 
-# Get the latest backup filename and path
-DB_BACKUP_NAME=$(ls $PATH_TO_BACKUPS | tail -n1)
-echo "Going to restore $DB_BACKUP_NAME"
-DB_BACKUP_PATH=$PATH_TO_BACKUPS/$DB_BACKUP_NAME
+db_name="${1:-$PGDATABASE}"
+backup_folder="${PATH_TO_BACKUPS}/backups/communaute"
+export RCLONE_CONFIG=${RCLONE_CONFIG:-"$PATH_TO_BACKUPS/rclone.conf"}
 
-echo "Going to inject DB_BACKUP_PATH=$DB_BACKUP_PATH"
-docker cp $DB_BACKUP_PATH commu_postgres:/backups
-
-echo "dropping current db"
-dropdb $PGDATABASE -U $PGUSER --if-exists --echo
-
-echo "creating new db"
-createdb $PGDATABASE -O $PGUSER -U $PGUSER --echo
-
-echo "restoring db"
-pg_restore -U $PGUSER --dbname=$PGDATABASE --format=c --clean --no-owner $DB_BACKUP_PATH
+rclone_last_backup="$(rclone lsf --files-only --max-age 24h communaute:/encrypted-backups)"
+rclone copy --max-age 24h --progress "communaute:/encrypted-backups/${rclone_last_backup}" "$backup_folder"
+backup_file="${backup_folder}/${rclone_last_backup}"
+echo "Restoring ${backup_file} to ${db_name} database"
+pg_restore --dbname="${db_name}" --format=c --clean --no-owner --jobs="$(nproc --all --ignore=1)" --verbose "${backup_file}"
+# Make sure we don't keep a copy for too long.
+rm "$backup_file"
+echo "Restoration is over!"
 
 echo "applying new migrations"
 python manage.py migrate
