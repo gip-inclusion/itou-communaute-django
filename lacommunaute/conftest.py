4f23620b1d12cefa31524cb49bd272596032b31a
Céline Martinet Sanchez <celine.martinet.sanchez+github@gmail.com>
2025-06-10 13:04:40 +0200

    Tests: add an admin client to auto-connect when needed

--- lacommunaute/conftest.py
+++ lacommunaute/conftest.py
@@ -1,8 +1,16 @@
 import pytest
 from django.core.management import call_command
 
+from lacommunaute.users.factories import UserFactory
+
 
 @pytest.fixture(scope="session", autouse=True)
 def run_compress(django_db_setup, django_db_blocker):
     with django_db_blocker.unblock():
         call_command("compress", "--force")
+
+
+@pytest.fixture
+def admin_client(client):
+    client.force_login(UserFactory(is_in_staff_group=True, is_superuser=True))
+    return client
