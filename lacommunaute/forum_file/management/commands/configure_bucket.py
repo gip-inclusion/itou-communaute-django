5c4fc7b4466ae73f7bd50e67167e9bc0687e2e04
François Freitag <mail@franek.fr>
2025-10-02 16:21:39 +0200

    configure_bucket: Put the bucket policy regardless of MinIO
    The buckets should always be created and be configured with the same
    policy as in production. Previously, only development buckets were
    managed through this command.

--- lacommunaute/forum_file/management/commands/configure_bucket.py
+++ lacommunaute/forum_file/management/commands/configure_bucket.py
@@ -1,8 +1,6 @@
 import json
-from urllib.parse import urljoin
 
 import boto3
-import httpx
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
@@ -20,37 +18,28 @@ def s3_client():
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        if self.check_minio():
-            client = s3_client()
+        client = s3_client()
 
-            for bucket_name in settings.AWS_STORAGE_BUCKET_NAME, settings.AWS_STORAGE_BUCKET_NAME_PUBLIC:
-                try:
-                    client.create_bucket(Bucket=bucket_name)
-                except client.exceptions.BucketAlreadyOwnedByYou:
-                    pass
+        for bucket_name in settings.AWS_STORAGE_BUCKET_NAME, settings.AWS_STORAGE_BUCKET_NAME_PUBLIC:
+            try:
+                client.create_bucket(Bucket=bucket_name)
+            except client.exceptions.BucketAlreadyOwnedByYou:
+                pass
 
-            # Set up public access to the AWS_STORAGE_BUCKET_NAME_PUBLIC bucket
-            public_bucket_policy = {
-                "Version": "2012-10-17",
-                "Statement": [
-                    {
-                        "Sid": "PublicRead",
-                        "Effect": "Allow",
-                        "Principal": "*",
-                        "Action": ["s3:GetObject"],
-                        "Resource": [f"arn:aws:s3:::{settings.AWS_STORAGE_BUCKET_NAME_PUBLIC}/*"],
-                    }
-                ],
-            }
-            client.put_bucket_policy(
-                Bucket=settings.AWS_STORAGE_BUCKET_NAME_PUBLIC, Policy=json.dumps(public_bucket_policy)
-            )
-
-    def check_minio(self):
-        # https://min.io/docs/minio/linux/operations/monitoring/healthcheck-probe.html#node-liveness
-        livecheck_url = urljoin(settings.AWS_S3_ENDPOINT_URL, "minio/health/live")
-        response = httpx.head(livecheck_url)
-        try:
-            return response.headers["Server"] == "MinIO"
-        except KeyError:
-            return False
+        # Set up public access to the AWS_STORAGE_BUCKET_NAME_PUBLIC bucket
+        public_bucket_policy = {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Sid": "PublicRead",
+                    "Effect": "Allow",
+                    "Principal": "*",
+                    "Action": ["s3:GetObject"],
+                    "Resource": [f"arn:aws:s3:::{settings.AWS_STORAGE_BUCKET_NAME_PUBLIC}/*"],
+                }
+            ],
+        }
+        client.put_bucket_policy(
+            Bucket=settings.AWS_STORAGE_BUCKET_NAME_PUBLIC,
+            Policy=json.dumps(public_bucket_policy),
+        )
