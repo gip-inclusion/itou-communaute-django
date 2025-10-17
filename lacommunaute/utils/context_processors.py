b7c175001ba652a51526666e2a70ab4039c7a21b
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2025-10-17 12:37:15 +0200

    utils: Add matomo context processor
    Mask ids in urls

--- lacommunaute/utils/context_processors.py
+++ lacommunaute/utils/context_processors.py
@@ -1,3 +1,5 @@
+from urllib.parse import urlencode
+
 from django.conf import settings
 
 
@@ -16,3 +18,18 @@ def expose_settings(request):
         "EMPLOIS_PRESCRIBER_SEARCH": settings.EMPLOIS_PRESCRIBER_SEARCH,
         "EMPLOIS_COMPANY_SEARCH": settings.EMPLOIS_COMPANY_SEARCH,
     }
+
+
+
+def matomo(request):
+    if not request.resolver_match:
+        return {"send_to_matomo": False}
+
+    context = {"send_to_matomo": True}
+    url = request.resolver_match.route
+    # Only keep Matomo-related params for now.
+    params = {k: v for k, v in request.GET.lists() if k.startswith(("utm_", "mtm_", "piwik_"))}
+    if params:
+        url = f"{url}?{urlencode(sorted(params.items()), doseq=True)}"
+    context["matomo_custom_url"] = url
+    return context
